5b5906a383b7f1b4caca7a192dd9c8a578e923ad
Liam Galvin <liam@liam-galvin.co.uk>
2019-03-05 08:22:03 +0000

    fixed release files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BINARY=tfsec
 TAG=${TRAVIS_TAG:-development}
-mkdir -p bin/darwin-amd64
-GOOS=darwin GOARCH=amd64 go build -o bin/darwin-amd64/${BINARY} -ldflags "-X github.com/liamg/tfsec/version.Version=${TAG}"
-mkdir -p bin/linux-amd64
-GOOS=linux GOARCH=amd64 go build -o bin/linux-amd64/${BINARY} -ldflags "-X github.com/liamg/tfsec/version.Version=${TAG}"
-mkdir -p bin/windows-amd64
-GOOS=windows GOARCH=amd64 go build -o bin/windows-amd64/${BINARY}.exe -ldflags "-X github.com/liamg/tfsec/version.Version=${TRAVIS_TAG}"
+mkdir -p bin/darwin
+GOOS=darwin GOARCH=amd64 go build -o bin/darwin/${BINARY}-darwin-amd64 -ldflags "-X github.com/liamg/tfsec/version.Version=${TAG}"
+mkdir -p bin/linux
+GOOS=linux GOARCH=amd64 go build -o bin/linux/${BINARY}-linux-amd64 -ldflags "-X github.com/liamg/tfsec/version.Version=${TAG}"
+mkdir -p bin/windows
+GOOS=windows GOARCH=amd64 go build -o bin/windows/${BINARY}-windows-amd64.exe -ldflags "-X github.com/liamg/tfsec/version.Version=${TRAVIS_TAG}"
