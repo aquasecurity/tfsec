e917a4f5fcae3482ea3b01eebdb8e09506dec76d
Liam Galvin <liam@liam-galvin.co.uk>
2019-10-29 20:07:06 +0000

    only build for linux for now

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,5 @@
 BINARY=tfsec
 TAG=${TRAVIS_TAG:-development}
 GO111MODULE=on
-mkdir -p bin/darwin
-GOOS=darwin GOARCH=amd64 go build -o bin/darwin/${BINARY}-darwin-amd64 -ldflags "-X github.com/liamg/tfsec/version.Version=${TAG}" ./cmd/tfsec/
 mkdir -p bin/linux
 GOOS=linux GOARCH=amd64 go build -o bin/linux/${BINARY}-linux-amd64 -ldflags "-X github.com/liamg/tfsec/version.Version=${TAG}" ./cmd/tfsec/
-mkdir -p bin/windows
-GOOS=windows GOARCH=amd64 go build -o bin/windows/${BINARY}-windows-amd64.exe -ldflags "-X github.com/liamg/tfsec/version.Version=${TRAVIS_TAG}" ./cmd/tfsec/
