d4920fa568ee11da3dd64b6173838ea593be0f16
Liam Galvin <liam@liam-galvin.co.uk>
2019-11-12 20:21:53 +0000

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,4 @@
 
 set -eux
 
-export GOPATH="$(pwd)/.tfsec_install"
-export GO111MODULE=on
-SRCDIR="${GOPATH}/src/github.com/liamg/tfsec"
-
-[ -d ${GOPATH} ] && rm -rf ${GOPATH}
-mkdir -p ${GOPATH}/{src,pkg,bin}
-mkdir -p ${SRCDIR}
-cp -r . ${SRCDIR}
-(
-    echo ${GOPATH}
-    cd ${SRCDIR}
-    go install ./cmd/tfsec
-)
+env GO111MODULE=on go install ./cmd/tfsec
