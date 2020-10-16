6d799087c38855e6ff4766cd5e823efa1f530f8a
Liam Galvin <liam@liam-galvin.co.uk>
2020-10-16 12:53:48 +0100

    Update aws001.go

--- internal/app/tfsec/checks/aws001.go
+++ internal/app/tfsec/checks/aws001.go
@@ -15,9 +15,9 @@ const AwsBadBucketACLDescription scanner.RuleSummary = "S3 Bucket has an ACL def
 const AWSBadBucketACLExplanation = `
 S3 bucket permissions should be set to deny public access unless explicitly required.
 
-Granting write access publicly with ` + "`" + `public-read-write` + "`" + ` is especially dangerous as you will be billed for any uploaded files.
+Granting write access publicly with <code>public-read-write</code> is especially dangerous as you will be billed for any uploaded files.
 
-Additionally, you should not use the 'authenticated-read' canned ACL, as this provides read access to any authenticated AWS user, not just AWS users within your organisation.
+Additionally, you should not use the <code>authenticated-read</code> canned ACL, as this provides read access to any authenticated AWS user, not just AWS users within your organisation.
 `
 const AWSBadBucketACLBadExample = `
 resource "aws_s3_bucket" "my-bucket" {
