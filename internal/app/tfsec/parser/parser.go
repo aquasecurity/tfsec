0902e366661c0eaad27fa407d092dfd905294c97
Liam Galvin <liam@liam-galvin.co.uk>
2020-10-22 08:53:18 +0100

    intelligently recurse directories

--- internal/app/tfsec/parser/parser.go
+++ internal/app/tfsec/parser/parser.go
@@ -2,6 +2,8 @@ package parser
 
 import (
 	"github.com/tfsec/tfsec/internal/app/tfsec/debug"
+	"io/ioutil"
+	"path/filepath"
 )
 
 // Parser is a tool for parsing terraform templates at a given file system location
@@ -21,24 +23,32 @@ func New(fullPath string, tfvarsPath string) *Parser {
 // ParseDirectory parses all terraform files within a given directory
 func (parser *Parser) ParseDirectory() (Blocks, error) {
 
-	debug.Log("Beginning parse for directory '%s'...", parser.fullPath)
-	files, err := LoadDirectory(parser.fullPath)
+	debug.Log("Finding Terraform subdirectories...")
+	subdirectories, err := parser.getSubdirectories(parser.fullPath)
 	if err != nil {
 		return nil, err
 	}
 
 	var blocks Blocks
 
-	for _, file := range files {
-		fileBlocks, err := LoadBlocksFromFile(file)
+	for _, dir := range subdirectories {
+		debug.Log("Beginning parse for directory '%s'...", parser.fullPath)
+		files, err := LoadDirectory(dir)
 		if err != nil {
 			return nil, err
 		}
-		if len(fileBlocks) > 0 {
-			debug.Log("Added %d blocks from %s...", len(fileBlocks), fileBlocks[0].DefRange.Filename)
-		}
-		for _, fileBlock := range fileBlocks {
-			blocks = append(blocks, NewBlock(fileBlock, nil, nil))
+
+		for _, file := range files {
+			fileBlocks, err := LoadBlocksFromFile(file)
+			if err != nil {
+				return nil, err
+			}
+			if len(fileBlocks) > 0 {
+				debug.Log("Added %d blocks from %s...", len(fileBlocks), fileBlocks[0].DefRange.Filename)
+			}
+			for _, fileBlock := range fileBlocks {
+				blocks = append(blocks, NewBlock(fileBlock, nil, nil))
+			}
 		}
 	}
 
@@ -57,4 +67,32 @@ func (parser *Parser) ParseDirectory() (Blocks, error) {
 	debug.Log("Evaluating expressions...")
 	evaluator := NewEvaluator(parser.fullPath, blocks, inputVars, modulesMetadata, modules)
 	return evaluator.EvaluateAll()
+}
+
+func (parser *Parser) getSubdirectories(path string) ([]string, error) {
+	entries, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() && filepath.Ext(entry.Name()) == ".tf" {
+			debug.Log("Found qualifying subdirctory containing .tf files: %s", path)
+			return []string{path}, nil
+		}
+	}
+
+	var results []string
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			dirs, err := parser.getSubdirectories(filepath.Join(path, entry.Name()))
+			if err != nil {
+				return nil, err
+			}
+			results = append(results, dirs...)
+		}
+	}
+
+	return results, nil
 }
