eef2a2951da391345572f7ce1c50c4e1df6a5044
Maas Lalani <maaslalani0@gmail.com>
2020-11-06 07:42:59 -0500

    Add GCP check to ensure that GKE node service accounts are overridden (#496)
    * Add gcp check to ensure that gke node service accounts are overridden
    
    * Slight change to use the isEmpty
    
    
    Co-authored-by: Owen Rumney <owen@owenrumney.co.uk>

---
+++ internal/app/tfsec/test/gcp012_test.go
@@ -0,0 +1,85 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/checks"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+func Test_GCPGKENodeServiceAccount(t *testing.T) {
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode scanner.RuleCode
+		mustExcludeResultCode scanner.RuleCode
+	}{
+		{
+			name: "does not define service account in container cluster",
+			source: `
+resource "google_container_cluster" "my-cluster" {
+  node_config {
+  }
+}
+`,
+			mustIncludeResultCode: checks.GCPGKENodeServiceAccount,
+		},
+		{
+			name: "does not define node_config in container cluster",
+			source: `
+resource "google_container_cluster" "my-cluster" {
+}
+`,
+			mustIncludeResultCode: checks.GCPGKENodeServiceAccount,
+		},
+		{
+			name: "defines service account in container cluster",
+			source: `
+resource "google_container_cluster" "my-cluster" {
+  node_config {
+    service_account = "anything"
+  }
+}
+`,
+			mustExcludeResultCode: checks.GCPGKENodeServiceAccount,
+		},
+		{
+			name: "does not define service account in container node pool",
+			source: `
+resource "google_container_node_pool" "my-np-cluster" {
+  node_config {
+  }
+}
+`,
+			mustIncludeResultCode: checks.GCPGKENodeServiceAccount,
+		},
+		{
+			name: "does not define node_config in container node pool",
+			source: `
+resource "google_container_node_pool" "my-np-cluster" {
+}
+`,
+			mustIncludeResultCode: checks.GCPGKENodeServiceAccount,
+		},
+		{
+			name: "defines service account in container node pool",
+			source: `
+resource "google_container_node_pool" "my-np-cluster" {
+  node_config {
+    service_account = "anything"
+  }
+}
+`,
+			mustExcludeResultCode: checks.GCPGKENodeServiceAccount,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			results := scanSource(test.source)
+			assertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
