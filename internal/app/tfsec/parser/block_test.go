b7da014e60f0a0217b3e3750dbe3f4374905ef70
Owen Rumney <owen@owenrumney.co.uk>
2020-11-09 17:16:30 +0000

    Rds performance insights encryption (#500)
    * Add Performance Insights encryption checks
    
    - ensure that if performance insights are enabled, an encryption key is
    provided
    
    * add MissingChild check on block

--- internal/app/tfsec/parser/block_test.go
+++ internal/app/tfsec/parser/block_test.go
@@ -94,6 +94,44 @@ resource "aws_s3_bucket" "my-bucket" {
 	}
 }
 
+func Test_MissingChildNotFoundOnBlock(t *testing.T) {
+	var tests = []struct {
+		name              string
+		source            string
+		expectedAttribute string
+	}{
+		{
+			name: "expected attribute is not present",
+			source: `
+resource "aws_s3_bucket" "my-bucket" {
+ 	bucket_name = "bucketName"
+	
+}`,
+			expectedAttribute: "acl",
+		},
+		{
+			name: "expected acl attribute is not present",
+			source: `
+resource "aws_s3_bucket" "my-bucket" {
+ 	bucket_name = "bucketName"
+	acl = "public-read"
+	
+}`,
+			expectedAttribute: "logging",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			blocks := createBlocksFromSource(test.source)
+			for _, block := range blocks {
+				assert.Equal(t, block.MissingChild(test.expectedAttribute), true)
+				assert.Equal(t, !block.HasChild(test.expectedAttribute), true)
+			}
+		})
+	}
+}
+
 func createBlocksFromSource(source string) []*Block {
 	path := createTestFile("test.tf", source)
 	blocks, err := New(filepath.Dir(path), "").ParseDirectory()
