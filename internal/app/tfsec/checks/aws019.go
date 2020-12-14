85db57918c4d0eb04df9f61120634c605d205530
Marcin Wyszynski <marcin.pixie@gmail.com>
2020-12-14 14:03:11 +0100

    AWS KMS key rotation not available for SIGN_VERIFY keys

--- internal/app/tfsec/checks/aws019.go
+++ internal/app/tfsec/checks/aws019.go
@@ -42,6 +42,11 @@ func init() {
 		RequiredLabels: []string{"aws_kms_key"},
 		CheckFunc: func(check *scanner.Check, block *parser.Block, _ *scanner.Context) []scanner.Result {
 			keyRotationAttr := block.GetAttribute("enable_key_rotation")
+			keyUsageAttr := block.GetAttribute("key_usage")
+
+			if keyUsageAttr.Value().AsString() == "SIGN_VERIFY" {
+				return nil
+			}
 
 			if keyRotationAttr == nil {
 				return []scanner.Result{
