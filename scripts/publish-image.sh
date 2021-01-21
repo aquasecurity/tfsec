5db99fbce43a73dbdaafbfd103a47b62649b7e04
Owen Rumney <owen@owenrumney.co.uk>
2021-01-21 09:46:58 +0000

    push a minor tag pointing to latest patch (#560)

--- scripts/publish-image.sh
+++ scripts/publish-image.sh
@@ -9,8 +9,15 @@ for IMAGE in ${IMAGES[@]}; do
     docker build --build-arg tfsec_version=${TRAVIS_TAG} -t ${IMAGE} .
 
     echo "publishing ${IMAGE}..."
+    # push the patch tag - eg; v0.36.15
     docker tag ${IMAGE} ${IMAGE}:${TRAVIS_TAG}
     docker push ${IMAGE}:${TRAVIS_TAG}
+
+    # push the minor tag - eg; v0.36
+    docker tag ${IMAGE} ${IMAGE}:
+    docker push ${IMAGE}:${TRAVIS_TAG%.*}
+
+    # push the latest tag
     docker tag ${IMAGE} ${IMAGE}:latest
     docker push ${IMAGE}:latest
 done;
