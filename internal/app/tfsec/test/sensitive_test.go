df8e7edc1389ea91df8dc5acf8940ab88b55688c
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2021-01-29 11:09:19 -0500

    Update sensitive_test.go

--- internal/app/tfsec/test/sensitive_test.go
+++ internal/app/tfsec/test/sensitive_test.go
@@ -19,7 +19,7 @@ func TestIsSensitiveAttribute(t *testing.T) {
 		{"bigcorp_eks_aux__externalsecrets_bingo_version", false},
 	}
 	for _, tt := range tests {
-		tt := tt // NOTE: https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel() // marks each test case as capable of running in parallel with each other
 			if security.IsSensitiveAttribute(tt.name) != tt.expected {
