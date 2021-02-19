b20f1b45fc127bf26a829059e3b2c0a17b86bee3
hkotka <henri.kotka@kaikuhealth.com>
2021-02-19 08:58:50 +0200

    Fixed whitespacing

--- internal/app/tfsec/test/azu017_test.go
+++ internal/app/tfsec/test/azu017_test.go
@@ -139,7 +139,7 @@ resource "azurerm_network_security_group" "example_deny" {
   
   security_rule {
      access                      = "Deny"
-	 source_port_range           = "any"
+     source_port_range           = "any"
      destination_port_range      = ["22", "80", "443"]
      source_address_prefix       = "*"
      destination_address_prefix  = "*"
