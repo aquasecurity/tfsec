47ec9013301ff5366dc3224f1bb8e722dcf292e9
Owen Rumney <owen@owenrumney.co.uk>
2021-03-18 20:13:20 +0000

    add check for network acl on azure key vault (#652)

---
+++ internal/app/tfsec/test/azu020_test.go
@@ -0,0 +1,75 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/checks"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+func Test_AZUKeyVaultNetworkAcl(t *testing.T) {
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode scanner.RuleCode
+		mustExcludeResultCode scanner.RuleCode
+	}{
+		{
+			name: "check fails when no network acl block is provided",
+			source: `
+resource "azurerm_key_vault" "bad_example" {
+    name                        = "examplekeyvault"
+    location                    = azurerm_resource_group.bad_example.location
+    enabled_for_disk_encryption = true
+    soft_delete_retention_days  = 7
+    purge_protection_enabled    = false
+}
+`,
+			mustIncludeResultCode: checks.AZUKeyVaultNetworkAcl,
+		},{
+			name: "check fails when network acl block is provided with default action as allow",
+			source: `
+resource "azurerm_key_vault" "bad_example" {
+    name                        = "examplekeyvault"
+    location                    = azurerm_resource_group.bad_example.location
+    enabled_for_disk_encryption = true
+    soft_delete_retention_days  = 7
+    purge_protection_enabled    = false
+
+    network_acls {
+        bypass = "AzureServices"
+        default_action = "Allow"
+    }
+}
+`,
+			mustIncludeResultCode: checks.AZUKeyVaultNetworkAcl,
+		},
+		{
+			name: "check passes when network acl is provided and default action is deny",
+			source: `
+resource "azurerm_key_vault" "good_example" {
+    name                        = "examplekeyvault"
+    location                    = azurerm_resource_group.good_example.location
+    enabled_for_disk_encryption = true
+    soft_delete_retention_days  = 7
+    purge_protection_enabled    = false
+
+    network_acls {
+        bypass = "AzureServices"
+        default_action = "Deny"
+    }
+}
+`,
+			mustExcludeResultCode: checks.AZUKeyVaultNetworkAcl,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			results := scanSource(test.source)
+			assertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
