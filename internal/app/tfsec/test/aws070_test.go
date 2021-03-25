85fd3956e9151a4d564147c8663da0d2bb22624f
Michal Franc <lam.michal.franc@gmail.com>
2021-03-25 21:03:22 +0100

    adding aws es domain no logging enabled scanner (#658)
    * adding aws es domain no logging enabled scanner
    
    Co-authored-by: Michal Franc <michal.franc@form3.tech>
    
    * adding AUDIT_LOGS verification
    
    Co-authored-by: Michal Franc <michal.franc@form3.tech>
    
    * minor changes to equality and spelling
    
    Co-authored-by: Michal Franc <michal.franc@form3.tech>
    
    * changed 057 and 070
    
    057 now iterates through the list of log options
    070 now checks only if one of the log options is Audit Log
    
    Co-authored-by: Michal Franc <michal.franc@form3.tech>

---
+++ internal/app/tfsec/test/aws070_test.go
@@ -0,0 +1,65 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/checks"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+func Test_AWSAWSESDomainShouldHaveAuditLogEnabled(t *testing.T) {
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode scanner.RuleCode
+		mustExcludeResultCode scanner.RuleCode
+	}{
+		{
+			name: "check fails if any of the log options dont specify log_type of AUDIT_LOGS",
+			source: `
+resource "aws_elasticsearch_domain" "example" {
+  // other config
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.example.arn
+    log_type                 = "SLOW_LOGS"
+  }
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.example.arn
+    log_type                 = "TEST_LOGS"
+  }
+}
+`,
+			mustIncludeResultCode: checks.AWSESDomainLoggingEnabled,
+		},
+		{
+			name: "check passes if one of the log_type is AUDIT_LOGS and audit log is enabled",
+			source: `
+resource "aws_elasticsearch_domain" "example" {
+  // other config
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.example.arn
+    log_type                 = "SLOW_LOGS"
+  }
+
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.example.arn
+    log_type                 = "AUDIT_LOGS"
+  }
+}
+`,
+			mustExcludeResultCode: checks.AWSESDomainLoggingEnabled,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			results := scanSource(test.source)
+			assertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
