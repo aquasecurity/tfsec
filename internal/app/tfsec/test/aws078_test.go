c2d8551bb12d1f544f9c6f888249a6bdc0ac2e44
Michal Franc <michal.franc@form3.tech>
2021-04-09 22:10:48 +0100

    add new check - for ecr image tab immutability

---
+++ internal/app/tfsec/test/aws078_test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/checks"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+func Test_AWSEnsureEcrImagesHaveImmutableTags(t *testing.T) {
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode scanner.RuleCode
+		mustExcludeResultCode scanner.RuleCode
+	}{
+		{
+			name: "should fire when image_tab_mutability attribute missing",
+			source: `
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+`,
+			mustIncludeResultCode: checks.AWSEnsureEcrImagesHaveImmutableTags,
+		},
+		{
+			name: "should fire when image_tab_mutability not set to IMMUTABLE",
+			source: `
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+`,
+			mustIncludeResultCode: checks.AWSEnsureEcrImagesHaveImmutableTags,
+		},
+		{
+			name: "should not fire when image_tab_mutability set to IMMUTABLE",
+			source: `
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+`,
+			mustExcludeResultCode: checks.AWSEnsureEcrImagesHaveImmutableTags,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			results := scanSource(test.source)
+			assertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
