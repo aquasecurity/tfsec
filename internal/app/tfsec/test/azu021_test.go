8d5665f169e86bd2be0b0fc711632cfa3fcaaa33
JP <48604544+jpmicrosoft@users.noreply.github.com>
2021-04-16 11:35:22 -0400

    Added additional test

--- internal/app/tfsec/test/azu021_test.go
+++ internal/app/tfsec/test/azu021_test.go
@@ -41,6 +41,32 @@ resource "azurerm_key_vault" "good_example" {
 `,
 			mustExcludeResultCode: checks.AZUKeyVaultPurgeProtection,
 		},
+		{
+			name: "check if purge_protection_enabled and soft_delete_retention_days is not set, check fails",
+			source: `
+resource "azurerm_key_vault" "bad_example" {
+    name                        = "examplekeyvault"
+    location                    = azurerm_resource_group.bad_example.location
+    enabled_for_disk_encryption = true
+    soft_delete_retention_days  = 0
+    purge_protection_enabled    = false
+}
+`,
+			mustIncludeResultCode: checks.AZUKeyVaultPurgeProtection,
+		},
+		{
+			name: "check if purge_protection_enabled is set but soft_delete_retention_days is not set, check fails",
+			source: `
+resource "azurerm_key_vault" "bad_example" {
+    name                        = "examplekeyvault"
+    location                    = azurerm_resource_group.bad_example.location
+    enabled_for_disk_encryption = true
+    soft_delete_retention_days  = 0
+    purge_protection_enabled    = true
+}
+`,
+			mustIncludeResultCode: checks.AZUKeyVaultPurgeProtection,
+		},		
 	}
 
 	for _, test := range tests {
