d5911389283eb10ef5f7ef00214b43fb61c354cc
JP <JP.Perez@microsoft.com>
2021-04-27 23:56:58 -0400

    Added AZU026 Ensure that the expiration date is set on all keys.

---
+++ internal/app/tfsec/test/azu026_test.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/checks"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+func Test_AZUKeyVaultKeyExpirationDate(t *testing.T) {
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode scanner.RuleCode
+		mustExcludeResultCode scanner.RuleCode
+	}{
+		{
+			name: "check if expiration_date is not set, check fails",
+			source: `
+resource "azurerm_key_vault_key" "bad_example" {
+  name         = "generated-certificate"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+}
+`,
+			mustIncludeResultCode: checks.AZUKeyVaultKeyExpirationDate,
+		},
+		{
+			name: "check if expiration_date is set, check passes",
+			source: `
+resource "azurerm_key_vault_key" "good_example" {
+  name         = "generated-certificate"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+  expiration_date = "1982-12-31T00:00:00Z"
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+}
+`,
+			mustExcludeResultCode: checks.AZUKeyVaultKeyExpirationDate,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			results := scanSource(test.source)
+			assertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
