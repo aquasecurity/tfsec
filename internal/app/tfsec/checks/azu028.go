4de0642207ae36d08f8ffec1dd74f35144a746ea
JP <JP.Perez@microsoft.com>
2021-05-04 09:21:02 -0400

    Added AZU028 Ensure the Function App can only be accessed via HTTPS. The default is false.

---
+++ internal/app/tfsec/checks/azu028.go
@@ -0,0 +1,81 @@
+package checks
+
+import (
+	"fmt"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/parser"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+const AZUFunctionAppHTTPS scanner.RuleCode = "AZU028"
+const AZUFunctionAppHTTPSDescription scanner.RuleSummary = "Ensure the Function App can only be accessed via HTTPS. The default is false."
+const AZUFunctionAppHTTPSExplanation = `
+By default, clients can connect to function endpoints by using both HTTP or HTTPS. You should redirect HTTP to HTTPs because HTTPS uses the SSL/TLS protocol to provide a secure connection, which is both encrypted and authenticated.
+`
+const AZUFunctionAppHTTPSBadExample = `
+resource "azurerm_function_app" "bad_example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+}
+`
+const AZUFunctionAppHTTPSGoodExample = `
+resource "azurerm_function_app" "good_example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+  https_only                 = true
+}
+`
+
+func init() {
+	scanner.RegisterCheck(scanner.Check{
+		Code: AZUFunctionAppHTTPS,
+		Documentation: scanner.CheckDocumentation{
+			Summary:     AZUFunctionAppHTTPSDescription,
+			Explanation: AZUFunctionAppHTTPSExplanation,
+			BadExample:  AZUFunctionAppHTTPSBadExample,
+			GoodExample: AZUFunctionAppHTTPSGoodExample,
+			Links: []string{
+				"https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#https_only",
+				"https://docs.microsoft.com/en-us/azure/app-service/configure-ssl-bindings#enforce-https",
+				"https://docs.microsoft.com/en-us/azure/azure-functions/security-concepts",
+			},
+		},
+		Provider:       scanner.AzureProvider,
+		RequiredTypes:  []string{"resource"},
+		RequiredLabels: []string{"azurerm_function_app"},
+		CheckFunc: func(check *scanner.Check, block *parser.Block, _ *scanner.Context) []scanner.Result {
+
+			if block.MissingChild("https_only") {
+				return []scanner.Result{
+					check.NewResult(
+						fmt.Sprintf("Resource '%s' should have https_only set to true, the default is false.", block.FullName()),
+						block.Range(),
+						scanner.SeverityError,
+					),
+				}
+			}
+			httpsonly := block.GetAttribute("https_only")
+			if httpsonly.IsFalse() {
+				return []scanner.Result{
+					check.NewResultWithValueAnnotation(
+						fmt.Sprintf("Resource '%s' should have https_only set to true, the default is false.", block.FullName()),
+						httpsonly.Range(),
+						httpsonly,
+						scanner.SeverityWarning,
+					),
+				}
+			}
+			return nil
+		},
+	})
+}
---
+++ internal/app/tfsec/test/azu028_test.go
@@ -0,0 +1,74 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tfsec/tfsec/internal/app/tfsec/checks"
+	"github.com/tfsec/tfsec/internal/app/tfsec/scanner"
+)
+
+func Test_AZUFunctionAppHTTPS(t *testing.T) {
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode scanner.RuleCode
+		mustExcludeResultCode scanner.RuleCode
+	}{
+		{
+			name: "check if https_only not set, check fails",
+			source: `
+resource "azurerm_function_app" "bad_example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+}
+`,
+			mustIncludeResultCode: checks.AZUFunctionAppHTTPS,
+		},
+		{
+			name: "check if https_only is set true, check passes",
+			source: `
+resource "azurerm_function_app" "good_example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+  https_only                 = true
+}
+`,
+			mustExcludeResultCode: checks.AZUFunctionAppHTTPS,
+		},
+		{
+			name: "check if managed_virtual_network_enabled is set to false, check fails",
+			source: `
+resource "azurerm_function_app" "bad_example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+  https_only                 = false
+}
+`,
+			mustIncludeResultCode: checks.AZUFunctionAppHTTPS,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			results := scanSource(test.source)
+			assertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
