67f5f4103faf81c07d775ccf4e05a3e2c0858708
Dat Tang <tangthanhdat6692@gmail.com>
2021-05-19 13:16:42 +0200

    Update name of AWS089

--- internal/app/tfsec/checks/aws089.go
+++ internal/app/tfsec/checks/aws089.go
@@ -8,8 +8,8 @@ import (
 )
 
 const AWSCloudWatchLogGroupsCMKEncrypted scanner.RuleCode = "AWS089"
-const AWSCloudWatchLogGroupsCMKEncryptedDescription scanner.RuleSummary = "CloudWatch log groups should be encrypted"
-const AWSCloudWatchLogGroupsCMKEncryptedImpact = "Log data may be leaked if the logs are compromised"
+const AWSCloudWatchLogGroupsCMKEncryptedDescription scanner.RuleSummary = "CloudWatch log groups should be encrypted using CMK"
+const AWSCloudWatchLogGroupsCMKEncryptedImpact = "Log data may be leaked if the logs are compromised. No auditing of who have viewed the logs."
 const AWSCloudWatchLogGroupsCMKEncryptedResolution = "Enable CMK encryption of CloudWatch Log Groups"
 const AWSCloudWatchLogGroupsCMKEncryptedExplanation = `
 CloudWatch log groups are encrypted by default, however, to get the full benefit of controlling key rotation and other KMS aspects a KMS CMK should be used.
