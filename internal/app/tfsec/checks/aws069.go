57dd5d610d31c53db02ac40bc9b3c70a14eae876
Owen Rumney <owen@owenrumney.co.uk>
2021-05-20 13:12:05 +0100

    Update aws069.go

--- internal/app/tfsec/checks/aws069.go
+++ internal/app/tfsec/checks/aws069.go
@@ -21,7 +21,7 @@ resource "aws_eks_cluster" "bad_example" {
     name = "bad_example_cluster"
     role_arn = var.cluster_arn
     vpc_config {
-        endpoint_public_access = true
+		endpoint_public_access = true
 		public_access_cidrs = ["0.0.0.0/0"]
     }
 }
