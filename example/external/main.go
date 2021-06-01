40e70a243e010eae649c113d42f6587585ac6432
Liam Galvin <liam@liam-galvin.co.uk>
2021-06-01 16:45:36 +0100

    fix example

--- example/external/main.go
+++ example/external/main.go
@@ -8,14 +8,18 @@ import (
 
 func main() {
 	scanner := externalscan.NewExternalScanner()
-	
-	_ = scanner.AddFile("../custom/custom_check.tf")
-	_ = scanner.AddFile("../custom/modules/public_custom_bucket/main.tf")
-	_ = scanner.AddFile("../good/good.tf")
-	_ = scanner.AddFile("../withVars/main.tf")
-	_ = scanner.AddFile("../withVars/variables.tf")
 
-	results, _ := scanner.Scan()
+	_ = scanner.AddPath("../custom/custom_check.tf")
+	_ = scanner.AddPath("../custom/modules/public_custom_bucket/main.tf")
+	_ = scanner.AddPath("../good/good.tf")
+	_ = scanner.AddPath("../withVars/main.tf")
+	_ = scanner.AddPath("../withVars/variables.tf")
+
+	results, err := scanner.Scan()
+	if err != nil {
+		panic(err)
+	}
+
 	for _, result := range results {
 		fmt.Printf("%s: %s\n", result.RuleID, result.Description)
 	}
