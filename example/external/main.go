cd06c5d869aeea0051da047c555cf23ad0677e62
Liam Galvin <liam@liam-galvin.co.uk>
2021-06-01 16:42:31 +0100

    Update main.go

--- example/external/main.go
+++ example/external/main.go
@@ -7,20 +7,15 @@ import (
 )
 
 func main() {
-	
 	scanner := externalscan.NewExternalScanner()
-
+	
 	_ = scanner.AddFile("../custom/custom_check.tf")
 	_ = scanner.AddFile("../custom/modules/public_custom_bucket/main.tf")
 	_ = scanner.AddFile("../good/good.tf")
 	_ = scanner.AddFile("../withVars/main.tf")
 	_ = scanner.AddFile("../withVars/variables.tf")
 
-	results, err := scanner.Scan()
-	if err != nil {
-		panic(err)
-	}
-
+	results, _ := scanner.Scan()
 	for _, result := range results {
 		fmt.Printf("%s: %s\n", result.RuleID, result.Description)
 	}
