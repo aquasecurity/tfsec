49207cf799d2efa47bfe5e37dca94b7283f11e7f
Owen Rumney <owen@owenrumney.co.uk>
2021-06-17 10:13:34 +0100

    Allow file output for text
    When the --out flag is used with the default or text formatter it has an
    empty file.
    
    - update text formatter to write to io.Writer
    - redirect default formatter to text when a file output is used

--- internal/app/tfsec/formatters/text.go
+++ internal/app/tfsec/formatters/text.go
@@ -6,15 +6,13 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/tfsec/tfsec/pkg/severity"
-
 	"github.com/tfsec/tfsec/pkg/result"
 )
 
-func FormatText(_ io.Writer, results []result.Result, _ string, options ...FormatterOption) error {
+func FormatText(writer io.Writer, results []result.Result, _ string, options ...FormatterOption) error {
 
 	if len(results) == 0 || len(results) == countPassedResults(results) {
-		fmt.Print("\nNo problems detected!\n")
+		fmt.Fprint(writer, "\nNo problems detected!\n")
 		return nil
 	}
 
@@ -28,7 +26,7 @@ func FormatText(_ io.Writer, results []result.Result, _ string, options ...Forma
 
 	var sev string
 
-	fmt.Printf("\n%d potential problems detected:\n\n", len(results)-countPassedResults(results))
+	fmt.Fprintf(writer, "\n%d potential problems detected:\n\n", len(results)-countPassedResults(results))
 	for i, res := range results {
 
 		var link string
@@ -36,28 +34,21 @@ func FormatText(_ io.Writer, results []result.Result, _ string, options ...Forma
 			link = res.Links[0]
 		}
 
-		fmt.Printf("Check %d\n", i+1)
+		fmt.Fprintf(writer, "Check %d\n", i+1)
 
 		if includePassedChecks && res.Passed() {
 			sev = "PASSED"
 		} else {
-			switch res.Severity {
-			case severity.Error:
-				sev = fmt.Sprintf("%s", res.Severity)
-			case severity.Warning:
-				sev = fmt.Sprintf("%s", res.Severity)
-			default:
-				sev = fmt.Sprintf("%s", res.Severity)
-			}
+			sev = string(res.Severity)
 		}
 
-		fmt.Printf(`
+		fmt.Fprintf(writer, `
   [%s][%s] %s
   %s
 
 `, res.RuleID, sev, res.Description, res.Range.String())
-		outputCode(res)
-		fmt.Printf("  %s\n\n", link)
+		outputCode(res, writer)
+		fmt.Fprintf(writer, "  %s\n\n", link)
 	}
 
 	return nil
@@ -65,7 +56,7 @@ func FormatText(_ io.Writer, results []result.Result, _ string, options ...Forma
 }
 
 // output the lines of code which caused a problem, if available
-func outputCode(result result.Result) {
+func outputCode(result result.Result, writer io.Writer) {
 	data, err := ioutil.ReadFile(result.Range.Filename)
 	if err != nil {
 		return
@@ -83,17 +74,17 @@ func outputCode(result result.Result) {
 	}
 
 	for lineNo := start; lineNo <= end; lineNo++ {
-		fmt.Printf("  % 6d | ", lineNo)
+		fmt.Fprintf(writer, "  % 6d | ", lineNo)
 		if lineNo >= result.Range.StartLine && lineNo <= result.Range.EndLine {
 			if lineNo == result.Range.StartLine && result.RangeAnnotation != "" {
-				fmt.Printf("%s    %s\n", lines[lineNo], result.RangeAnnotation)
+				fmt.Fprintf(writer, "%s    %s\n", lines[lineNo], result.RangeAnnotation)
 			} else {
-				fmt.Printf("%s\n", lines[lineNo])
+				fmt.Fprintf(writer, "%s\n", lines[lineNo])
 			}
 		} else {
-			fmt.Printf("%s\n", lines[lineNo])
+			fmt.Fprintf(writer, "%s\n", lines[lineNo])
 		}
 	}
 
-	fmt.Println("")
+	fmt.Fprintln(writer, "")
 }
