13aa0e8e81dbcaf79eda6fea6b1710c9675a60e4
Teppei Fukuda <knqyf263@gmail.com>
2021-06-24 12:45:33 +0300

    Update internal/app/tfsec/scanner/scanner.go
    Co-authored-by: Owen Rumney <owen@owenrumney.co.uk>

--- internal/app/tfsec/scanner/scanner.go
+++ internal/app/tfsec/scanner/scanner.go
@@ -64,7 +64,7 @@ func (scanner *Scanner) Scan(blocks []*block.Block) []result.Result {
 					debug.Log("Running rule for %s on %s.%s (%s)...", r.ID, checkBlock.Type(), checkBlock.FullName(), checkBlock.Range().Filename)
 					ruleResults := rule.CheckRule(r, checkBlock, context)
 					if scanner.includePassed && ruleResults.All() == nil {
-						res := result.New(checkBlock).WithRuleID(r.ID).WithDescription(r.Documentation.Summary).
+						res := result.New(checkBlock).WithRuleID(r.ID).WithDescription(fmt.Sprintf("Resource '%s' passed check: %s", checkBlock.FullName(), r.Documentation.Summary)).
 							WithRange(checkBlock.Range()).WithStatus(result.Passed).WithSeverity(severity.None)
 						results = append(results, *res)
 					} else if ruleResults != nil {
