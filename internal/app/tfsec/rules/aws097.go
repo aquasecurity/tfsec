1c0f7c0e428d61f5fdbdc3a8a6632ef25d191081
Liam Galvin <liam@liam-galvin.co.uk>
2021-07-08 10:51:12 +0100

    Allow KMS actions for all resources for non-IAM policy documents (#832)
    * Allow KMS actions for all resources for non-IAM policy documents

--- internal/app/tfsec/rules/aws097.go
+++ internal/app/tfsec/rules/aws097.go
@@ -1,7 +1,9 @@
 package rules
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/tfsec/tfsec/pkg/severity"
 
@@ -26,6 +28,30 @@ const AWSKMSManagedPoliciesShouldNotAllowDecryptionActionsOnAllKMSKeysExplanatio
 IAM policies define which actions an identity (user, group, or role) can perform on which resources. Following security best practices, AWS recommends that you allow least privilege. In other words, you should grant to identities only the kms:Decrypt or kms:ReEncryptFrom permissions and only for the keys that are required to perform a task.
 `
 const AWSKMSManagedPoliciesShouldNotAllowDecryptionActionsOnAllKMSKeysBadExample = `
+resource "aws_iam_role_policy" "test_policy" {
+	name = "test_policy"
+	role = aws_iam_role.test_role.id
+
+	policy = data.aws_iam_policy_document.kms_policy.json
+}
+
+resource "aws_iam_role" "test_role" {
+	name = "test_role"
+	assume_role_policy = jsonencode({
+		Version = "2012-10-17"
+		Statement = [
+		{
+			Action = "sts:AssumeRole"
+			Effect = "Allow"
+			Sid    = ""
+			Principal = {
+			Service = "ec2.amazonaws.com"
+			}
+		},
+		]
+	})
+}
+
 data "aws_iam_policy_document" "kms_policy" {
   statement {
     principals {
@@ -38,6 +64,30 @@ data "aws_iam_policy_document" "kms_policy" {
 }
 `
 const AWSKMSManagedPoliciesShouldNotAllowDecryptionActionsOnAllKMSKeysGoodExample = `
+resource "aws_iam_role_policy" "test_policy" {
+	name = "test_policy"
+	role = aws_iam_role.test_role.id
+  
+	policy = data.aws_iam_policy_document.kms_policy.json
+}
+
+resource "aws_iam_role" "test_role" {
+	name = "test_role"
+	assume_role_policy = jsonencode({
+		Version = "2012-10-17"
+		Statement = [
+		{
+			Action = "sts:AssumeRole"
+			Effect = "Allow"
+			Sid    = ""
+			Principal = {
+			Service = "ec2.amazonaws.com"
+			}
+		},
+		]
+	})
+}
+
 data "aws_iam_policy_document" "kms_policy" {
   statement {
     principals {
@@ -49,6 +99,46 @@ data "aws_iam_policy_document" "kms_policy" {
   }
 }
 `
+
+type awsIAMPolicyDocument struct {
+	Statements []awsIAMPolicyDocumentStatement `json:"Statement"`
+}
+
+type awsIAMPolicyDocumentStatement struct {
+	Effect   string                    `json:"Effect"`
+	Action   awsIAMPolicyDocumentValue `json:"Action"`
+	Resource awsIAMPolicyDocumentValue `json:"Resource,omitempty"`
+}
+
+// AWS allows string or []string as value, we convert everything to []string to avoid casting
+type awsIAMPolicyDocumentValue []string
+
+func (value *awsIAMPolicyDocumentValue) UnmarshalJSON(b []byte) error {
+
+	var raw interface{}
+	err := json.Unmarshal(b, &raw)
+	if err != nil {
+		return err
+	}
+
+	var p []string
+	//  value can be string or []string, convert everything to []string
+	switch v := raw.(type) {
+	case string:
+		p = []string{v}
+	case []interface{}:
+		var items []string
+		for _, item := range v {
+			items = append(items, fmt.Sprintf("%v", item))
+		}
+		p = items
+	default:
+		return fmt.Errorf("invalid %s value element: allowed is only string or []string", value)
+	}
+
+	*value = p
+	return nil
+}
 
 func init() {
 	scanner.RegisterCheckRule(rule.Rule{
@@ -65,12 +155,56 @@ func init() {
 			},
 		},
 		Provider:        provider.AWSProvider,
-		RequiredTypes:   []string{"data"},
-		RequiredLabels:  []string{"aws_iam_policy_document"},
+		RequiredTypes:   []string{"resource"},
+		RequiredLabels:  []string{"aws_iam_policy", "aws_iam_group_policy", "aws_iam_user_policy", "aws_iam_role_policy"},
 		DefaultSeverity: severity.Error,
-		CheckFunc: func(set result.Set, resourceBlock block.Block, _ *hclcontext.Context) {
+		CheckFunc: func(set result.Set, resourceBlock block.Block, ctx *hclcontext.Context) {
 
-			if statementBlocks := resourceBlock.GetBlocks("statement"); statementBlocks != nil {
+			policyAttr := resourceBlock.GetAttribute("policy")
+			if policyAttr == nil {
+				return
+			}
+
+			if policyAttr.IsString() {
+				var document awsIAMPolicyDocument
+				if err := json.Unmarshal([]byte(policyAttr.Value().AsString()), &document); err != nil {
+					return
+				}
+				for _, statement := range document.Statements {
+					if strings.ToLower(statement.Effect) == "deny" {
+						continue
+					}
+					for _, action := range statement.Action {
+						if !strings.HasPrefix(action, "kms:") {
+							continue
+						}
+						for _, resource := range statement.Resource {
+							if strings.Contains(resource, "*") {
+								set.Add(
+									result.New(resourceBlock).
+										WithDescription(fmt.Sprintf("Resource '%s' a policy with KMS actions for all KMS keys.", resourceBlock.FullName())).
+										WithRange(policyAttr.Range()).
+										WithAttributeAnnotation(policyAttr).
+										WithSeverity(severity.Error),
+								)
+								return
+							}
+						}
+					}
+				}
+				return
+			}
+
+			policyDocumentBlock, err := ctx.GetReferencedBlock(policyAttr)
+			if err != nil {
+				return
+			}
+
+			if policyDocumentBlock.Type() != "data" || policyDocumentBlock.TypeLabel() != "aws_iam_policy_document" {
+				return
+			}
+
+			if statementBlocks := policyDocumentBlock.GetBlocks("statement"); statementBlocks != nil {
 				for _, statementBlock := range statementBlocks {
 
 					// Denying a broad set of KMS access is fine
@@ -84,7 +218,7 @@ func init() {
 								set.Add(
 									result.New(resourceBlock).
 										WithDescription(fmt.Sprintf("Resource '%s' a policy with KMS actions for all KMS keys.", resourceBlock.FullName())).
-										WithRange(resourceBlock.Range()).
+										WithRange(resources.Range()).
 										WithAttributeAnnotation(resources).
 										WithSeverity(severity.Error),
 								)
