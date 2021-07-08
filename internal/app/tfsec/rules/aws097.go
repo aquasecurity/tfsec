65f7896d8ed9fc92921a37ed78fd5b387ca5efdc
Liam Galvin <liam@liam-galvin.co.uk>
2021-07-08 10:27:35 +0100

    Update aws097.go

--- internal/app/tfsec/rules/aws097.go
+++ internal/app/tfsec/rules/aws097.go
@@ -70,8 +70,6 @@ resource "aws_iam_role_policy" "test_policy" {
 	name = "test_policy"
 	role = aws_iam_role.test_role.id
   
-	# Terraform's "jsonencode" function converts a
-	# Terraform expression result to valid JSON syntax.
 	policy = data.aws_iam_policy_document.kms_policy.json
 }
 
