ee3a2210ac9346e7179dc52ce666006c7c50c18b
Liam Galvin <liam.galvin@aquasec.com>
2021-07-19 16:32:51 +0100

    move fixes for for_each to v1 branch

--- internal/app/tfsec/scanner/registration.go
+++ internal/app/tfsec/scanner/registration.go
@@ -2,7 +2,6 @@ package scanner
 
 import (
 	"fmt"
-	"os"
 	"sort"
 	"sync"
 
@@ -14,21 +13,14 @@ var registeredRules []rule.Rule
 
 // RegisterCheckRule registers a new Rule which should be run on future scans
 func RegisterCheckRule(rule rule.Rule) {
-	if rule.ShortCode == "" {
-		panic("rule short code was not set")
-	}
-	if rule.Service == "" {
-		panic("rule service was not set")
-	}
-	if rule.Provider == "" {
-		panic("rule provider was not set")
+	if rule.ID == "" {
+		panic("rule code was not set")
 	}
 	rulesLock.Lock()
 	defer rulesLock.Unlock()
 	for _, existing := range registeredRules {
-		if existing.ID() == rule.ID() {
-			fmt.Fprintf(os.Stderr, "Error: rule already exists with code '%s'\n", rule.ID())
-			os.Exit(1)
+		if existing.ID == rule.ID {
+			panic(fmt.Errorf("rule already exists with code '%s'", rule.ID))
 		}
 	}
 	registeredRules = append(registeredRules, rule)
@@ -37,7 +29,7 @@ func RegisterCheckRule(rule rule.Rule) {
 // GetRegisteredRules provides all Checks which have been registered with this package
 func GetRegisteredRules() []rule.Rule {
 	sort.Slice(registeredRules, func(i, j int) bool {
-		return registeredRules[i].ID() < registeredRules[j].ID()
+		return registeredRules[i].ID < registeredRules[j].ID
 	})
 	return registeredRules
 }
