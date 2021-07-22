4a116a62d28c0553c1910bcdc12f4d80d164b82c
Liam Galvin <liam.galvin@aquasec.com>
2021-07-22 13:34:55 +0100

    Add postgres minimum log severity check

---
+++ internal/app/tfsec/rules/google/sql/pg_log_errors_rule.go
@@ -0,0 +1,93 @@
+package sql
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aquasecurity/tfsec/pkg/result"
+	"github.com/aquasecurity/tfsec/pkg/severity"
+
+	"github.com/aquasecurity/tfsec/pkg/provider"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/hclcontext"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
+
+	"github.com/aquasecurity/tfsec/pkg/rule"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
+)
+
+func init() {
+	scanner.RegisterCheckRule(rule.Rule{
+		Service:   "sql",
+		ShortCode: "pg-log-errors",
+		Documentation: rule.RuleDocumentation{
+			Summary:     "Ensure that Postgres errors are logged",
+			Explanation: `Setting the minimum log severity too high will cause errors not to be logged`,
+			Impact:      "Loss of error logging",
+			Resolution:  "Set the minimum log severity to at least ERROR",
+			BadExample: `
+resource "google_sql_database_instance" "db" {
+	name             = "db"
+	database_version = "POSTGRES_12"
+	region           = "us-central1"
+	settings {
+		database_flags {
+			name  = "log_min_messages"
+			value = "PANIC"
+		}
+	}
+}
+			`,
+			GoodExample: `
+resource "google_sql_database_instance" "db" {
+	name             = "db"
+	database_version = "POSTGRES_12"
+	region           = "us-central1"
+	settings {
+		database_flags {
+			name  = "log_min_messages"
+			value = "WARNING"
+		}
+	}
+}
+			`,
+			Links: []string{
+				"https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance",
+				"https://postgresqlco.nf/doc/en/param/log_min_messages/",
+				"https://www.postgresql.org/docs/13/runtime-config-logging.html#GUC-LOG-MIN-MESSAGES",
+			},
+		},
+		Provider:        provider.GoogleProvider,
+		RequiredTypes:   []string{"resource"},
+		RequiredLabels:  []string{"google_sql_database_instance"},
+		DefaultSeverity: severity.Low,
+		CheckFunc: func(set result.Set, resourceBlock block.Block, _ *hclcontext.Context) {
+			dbVersionAttr := resourceBlock.GetAttribute("database_version")
+			if dbVersionAttr != nil && dbVersionAttr.IsString() && !strings.HasPrefix(dbVersionAttr.Value().AsString(), "POSTGRES") {
+				return
+			}
+
+			settingsBlock := resourceBlock.GetBlock("settings")
+			if settingsBlock == nil {
+				return
+			}
+
+			for _, dbFlagBlock := range settingsBlock.GetBlocks("database_flags") {
+				if nameAttr := dbFlagBlock.GetAttribute("name"); nameAttr != nil && nameAttr.IsString() && nameAttr.Value().AsString() == "log_min_messages" {
+					if valueAttr := dbFlagBlock.GetAttribute("value"); valueAttr != nil && valueAttr.IsString() {
+						switch valueAttr.Value().AsString() {
+						case "FATAL", "PANIC", "LOG":
+							set.Add(
+								result.New(resourceBlock).
+									WithDescription(fmt.Sprintf("Resource '%s' has a minimum log severity set which ignores errors", resourceBlock.FullName())),
+							)
+						}
+					}
+				}
+			}
+
+		},
+	})
+}
