8b8072dbacb379026bff1a42c0a1a9116bedaa22
Owen Rumney <owen.rumney@aquasec.com>
2021-07-26 14:43:04 +0100

    add additional resources this applies to (#949)

--- internal/app/tfsec/rules/azure/database/no_public_firewall_access_rule_test.go
+++ internal/app/tfsec/rules/azure/database/no_public_firewall_access_rule_test.go
@@ -18,10 +18,23 @@ func Test_AzureNoPublicFirewallAccess(t *testing.T) {
 		{
 			name: "rule with open ip range fails check",
 			source: `
-resource "azurerm_sql_firewall_rule" "bad_example" {
-  name                = "bad_rule"
+resource "azurerm_postgresql_firewall_rule" "bad_example" {
+  name                = "bad_example"
   resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_sql_server.example.name
+  server_name         = azurerm_postgresql_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+`,
+			mustIncludeResultCode: expectedCode,
+		},
+		{
+			name: "postgresql rule with open ip range fails check",
+			source: `
+resource "azurerm_postgresql_firewall_rule" "bad_example" {
+  name                = "bad_example"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_postgresql_server.example.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
 }
@@ -51,6 +64,14 @@ resource "azurerm_sql_firewall_rule" "good_example" {
   start_ip_address    = "10.0.2.0"
   end_ip_address      = "10.0.2.240"
 }
+
+resource "azurerm_postgresql_firewall_rule" "good_example" {
+	name                = "good_rule"
+	resource_group_name = azurerm_resource_group.example.name
+	server_name         = azurerm_sql_server.example.name
+	start_ip_address    = "10.0.2.0"
+	end_ip_address      = "10.0.2.240"
+  }
 `,
 			mustExcludeResultCode: expectedCode,
 		},
