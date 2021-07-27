19927065aa2aaf50ba7e5b19681a78a8e1daece9
Liam Galvin <liam.galvin@aquasec.com>
2021-07-27 17:06:51 +0100

    Add remaining google_project_iam_member rules

---
+++ internal/app/tfsec/rules/google/iam/no_project_level_service_account_impersonation_rule.go
@@ -0,0 +1,74 @@
+package iam
+
+import (
+	"fmt"
+
+	"github.com/aquasecurity/tfsec/pkg/result"
+	"github.com/aquasecurity/tfsec/pkg/severity"
+
+	"github.com/aquasecurity/tfsec/pkg/provider"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/hclcontext"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
+
+	"github.com/aquasecurity/tfsec/pkg/rule"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
+)
+
+func init() {
+	scanner.RegisterCheckRule(rule.Rule{
+		Service:   "iam",
+		ShortCode: "no-project-level-service-account-impersonation",
+		Documentation: rule.RuleDocumentation{
+			Summary:     "Users should not be granted service account access at the project level",
+			Explanation: `Users with service account access at project level can impersonate any service account. Instead, they should be given access to particular service accounts as required.`,
+			Impact:      "Privilege escalation, impersonation of any/all services",
+			Resolution:  "Provide access at the service-level instead of project-level, if required",
+			BadExample: []string{
+				`
+resource "google_project_iam_binding" "project-123" {
+	project = "project-123"
+	role    = "roles/iam.serviceAccountUser"
+}
+`,
+				`
+resource "google_project_iam_binding" "project-123" {
+	project = "project-123"
+	role    = "roles/iam.serviceAccountTokenCreator"
+}
+`,
+			},
+			GoodExample: []string{`
+resource "google_project_iam_binding" "project-123" {
+	project = "project-123"
+	role    = "roles/nothingInParticular"
+}
+			`},
+			Links: []string{
+				"https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam",
+				"https://cloud.google.com/iam/docs/impersonating-service-accounts",
+			},
+		},
+		Provider:        provider.GoogleProvider,
+		RequiredTypes:   []string{"resource"},
+		RequiredLabels:  []string{"google_project_iam_binding", "google_project_iam_member"},
+		DefaultSeverity: severity.Medium,
+		CheckFunc: func(set result.Set, resourceBlock block.Block, _ *hclcontext.Context) {
+
+			roleAttr := resourceBlock.GetAttribute("role")
+			if roleAttr == nil || !roleAttr.IsString() || !roleAttr.Value().IsKnown() {
+				return
+			}
+			if roleAttr.Value().AsString() == "roles/iam.serviceAccountUser" || roleAttr.Value().AsString() == "roles/iam.serviceAccountTokenCreator" {
+				set.Add(
+					result.New(resourceBlock).
+						WithAttribute(roleAttr).
+						WithDescription(fmt.Sprintf("Resource '%s' grants service account access to a user at project level.", resourceBlock.FullName())),
+				)
+			}
+
+		},
+	})
+}
