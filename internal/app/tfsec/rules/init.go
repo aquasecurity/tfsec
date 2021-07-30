b1ae477d922efcec127dbe58d219edaed52652f1
Liam Galvin <liam.galvin@aquasec.com>
2021-07-30 16:48:29 +0100

    Add k8s to init

--- internal/app/tfsec/rules/init.go
+++ internal/app/tfsec/rules/init.go
@@ -63,6 +63,7 @@ import (
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/google/iam"
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/google/sql"
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/google/storage"
+	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/kubernetes/network"
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/openstack/compute"
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/openstack/fw"
 	_ "github.com/aquasecurity/tfsec/internal/app/tfsec/rules/oracle/compute"
