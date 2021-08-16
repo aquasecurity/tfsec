e513091b2e293124b3842cb6622fe6ab02045d54
Owen Rumney <owen@owenrumney.co.uk>
2021-08-16 16:57:28 +0100

    Redshift SG needs description

---
+++ internal/app/tfsec/rules/aws/redshift/add_decription_to_security_group_rule_test.go
@@ -0,0 +1,45 @@
+package redshift
+
+// generator-locked
+import (
+	"testing"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/testutil"
+)
+
+func Test_AWSMissingDescriptionForSecurityGroup(t *testing.T) {
+	expectedCode := "aws-redshift-add-decription-to-security-group"
+
+	var tests = []struct {
+		name                  string
+		source                string
+		mustIncludeResultCode string
+		mustExcludeResultCode string
+	}{
+		{
+			name: "check aws_security_group without description",
+			source: `
+resource "aws_redshift_security_group" "my-group" {
+	
+}`,
+			mustIncludeResultCode: expectedCode,
+		},
+		{
+			name: "check aws_security_group with description",
+			source: `
+resource "aws_redshift_security_group" "my-group" {
+	description = "this is a group for allowing shiz"
+}`,
+			mustExcludeResultCode: expectedCode,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			results := testutil.ScanHCL(test.source, t)
+			testutil.AssertCheckCode(t, test.mustIncludeResultCode, test.mustExcludeResultCode, results)
+		})
+	}
+
+}
