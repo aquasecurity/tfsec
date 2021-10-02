3ae59d1f9199c9145ce9da78a071cd5bef4421c3
Brandon Daniel Bailey <brandondanielbailey@gmail.com>
2021-10-02 17:19:10 -0400

    Mysql Threat detection and required email security center check (#1095)
    * Mysql Threat detection and required email security center check
    * Update mysql_threat_detection_enabled_rule.go
    
    Co-authored-by: Brandon Bailey <81580001+brandon-bailey-rc@users.noreply.github.com>

---
+++ internal/app/tfsec/rules/azure/database/mysql_threat_detection_enabled_rule_test.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/testutil"
+)
+
+func Test_DatabaseMysqlThreatDetectionEnabled_FailureExamples(t *testing.T) {
+	expectedCode := "azure-database-mysql-threat-detection-enabled"
+
+	rule, err := scanner.GetRuleById(expectedCode)
+	if err != nil {
+		t.Fatalf("Rule not found: %s", expectedCode)
+	}
+	for i, badExample := range rule.Documentation.BadExample {
+		t.Logf("Running bad example for '%s' #%d", expectedCode, i+1)
+		if strings.TrimSpace(badExample) == "" {
+			t.Fatalf("bad example code not provided for %s", rule.ID())
+		}
+		defer func() {
+			if err := recover(); err != nil {
+				t.Fatalf("Scan (bad) failed: %s", err)
+			}
+		}()
+		results := testutil.ScanHCL(badExample, t)
+		testutil.AssertCheckCode(t, rule.ID(), "", results)
+	}
+}
+
+func Test_DatabaseMysqlThreatDetectionEnabled_SuccessExamples(t *testing.T) {
+	expectedCode := "azure-database-mysql-threat-detection-enabled"
+
+	rule, err := scanner.GetRuleById(expectedCode)
+	if err != nil {
+		t.Fatalf("Rule not found: %s", expectedCode)
+	}
+	for i, example := range rule.Documentation.GoodExample {
+		t.Logf("Running good example for '%s' #%d", expectedCode, i+1)
+		if strings.TrimSpace(example) == "" {
+			t.Fatalf("good example code not provided for %s", rule.ID())
+		}
+		defer func() {
+			if err := recover(); err != nil {
+				t.Fatalf("Scan (good) failed: %s", err)
+			}
+		}()
+		results := testutil.ScanHCL(example, t)
+		testutil.AssertCheckCode(t, "", rule.ID(), results)
+	}
+}
