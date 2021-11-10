7e804b0f848fe1a1094b0c6067e221104dfdc4a7
Liam Galvin <liam.galvin@aquasec.com>
2021-11-10 10:11:26 +0000

    Update scanner.go

--- internal/app/tfsec/scanner/scanner.go
+++ internal/app/tfsec/scanner/scanner.go
@@ -90,7 +90,7 @@ func (scanner *Scanner) scanModule(module block.Module, rules []rule.Rule) []res
 							ruleResult.Severity = r.DefaultSeverity
 						}
 						if reference := checkBlock.Reference(); reference != nil {
-							ruleResult.Resource = checkBlock.Reference().String()
+							ruleResult.Resource = reference.String()
 						}
 						if len(scanner.includedRuleIDs) == 0 || len(scanner.includedRuleIDs) > 0 && checkInList(ruleResult.RuleID, ruleResult.LegacyRuleID, scanner.includedRuleIDs) {
 							if !scanner.includeIgnored && (ruleResult.IsIgnored(scanner.workspaceName) || checkInList(ruleResult.RuleID, ruleResult.LegacyRuleID, scanner.excludedRuleIDs)) {
