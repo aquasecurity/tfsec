a46b3316d1f68f11b77da91dc9867171792476a3
PureLogics <usman.sattar@purelogics.net>
2021-12-03 17:13:34 +0500

    Fixes in delete_retention_policy_rule.go added new more bad Examples for this .

--- internal/app/tfsec/rules/azure/storage/delete_retention_policy_rule.go
+++ internal/app/tfsec/rules/azure/storage/delete_retention_policy_rule.go
@@ -63,14 +63,9 @@ func init() {
 				  environment = "staging"
 				}
 				blob_properties {
-					delete_retention_policy {
-						days = 0
-					}
-					container_delete_retention_policy{
-						days = 0
-					  } 
 				}  
-			  }`, `resource "azurerm_resource_group" "example" {
+			  }`,
+				`resource "azurerm_resource_group" "example" {
 				name     = "example-resources"
 				location = "West Europe"
 			  }
@@ -108,15 +103,52 @@ func init() {
 				tags = {
 				  environment = "staging"
 				}
-			   blob_properties {
-				   delete_retention_policy {
-					   days = 120
-				   }
-				   container_delete_retention_policy {
-					days = 1
-				  } 
-			   } 
-				   }`,
+			 
+				   }`, `
+				   			resource "azurerm_resource_group" "example" {
+					name     = "example-resources"
+					location = "West Europe"
+				  }
+				  
+				  resource "azurerm_virtual_network" "example" {
+					name                = "virtnetname"
+					address_space       = ["10.0.0.0/16"]
+					location            = azurerm_resource_group.example.location
+					resource_group_name = azurerm_resource_group.example.name
+				  }
+				  
+				  resource "azurerm_subnet" "example" {
+					name                 = "subnetname"
+					resource_group_name  = azurerm_resource_group.example.name
+					virtual_network_name = azurerm_virtual_network.example.name
+					address_prefixes     = ["10.0.2.0/24"]
+					service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+				  }
+				  
+				  resource "azurerm_storage_account" "example" {
+					name                = "storageaccountname"
+					resource_group_name = azurerm_resource_group.example.name
+				  
+					location                 = azurerm_resource_group.example.location
+					account_tier             = "Standard"
+					account_replication_type = "LRS"
+					min_tls_version          = "TLS1_2"
+				  
+					network_rules {
+					  default_action             = "Deny"
+					  ip_rules                   = ["100.0.0.1"]
+					  virtual_network_subnet_ids = [azurerm_subnet.example.id]
+					}
+				  
+					tags = {
+					  environment = "staging"
+					}
+					blob_properties {
+						container_delete_retention_policy {
+							days = 7
+						}
+						}  
+				  }`,
 			},
 			GoodExample: []string{`
 				resource "azurerm_resource_group" "example" {
