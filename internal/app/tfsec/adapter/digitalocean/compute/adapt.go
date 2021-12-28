d44bd1ff6d2a477a631a651a926e9340884f4e43
Mads Jensen <mje@inducks.org>
2021-12-28 12:03:41 +0100

    DigitalOcean SSH keys check

--- internal/app/tfsec/adapter/digitalocean/compute/adapt.go
+++ internal/app/tfsec/adapter/digitalocean/compute/adapt.go
@@ -6,5 +6,26 @@ import (
 )
 
 func Adapt(modules []block.Module) compute.Compute {
-	return compute.Compute{}
+	return compute.Compute{
+		Droplets: adaptDroplets(modules),
+	}
+}
+
+func adaptDroplets(module block.Modules) []compute.Droplet {
+	var droplets []compute.Droplet
+
+	for _, module := range module {
+		for _, block := range module.GetResourcesByType("digitalocean_droplet") {
+			droplet := compute.Droplet{
+				Metadata: *(block.GetMetadata()),
+			}
+			sshKeys := block.GetAttribute("ssh_keys").GetRawValue()
+			if sshKeys == nil {
+				droplet.SSHKeys = nil
+			}
+
+			droplets = append(droplets, droplet)
+		}
+	}
+	return droplets
 }
