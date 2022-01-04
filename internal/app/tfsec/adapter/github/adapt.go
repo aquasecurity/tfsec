87ffc3a105779addb8d5eb175fe1a6dcb006836b
Liam Galvin <liam.galvin@aquasec.com>
2022-01-04 13:15:32 +0000

    Add check for plaintext github action secrets

--- internal/app/tfsec/adapter/github/adapt.go
+++ internal/app/tfsec/adapter/github/adapt.go
@@ -3,11 +3,13 @@ package github
 import (
 	"github.com/aquasecurity/defsec/provider/github"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/adapter/github/repositories"
+	"github.com/aquasecurity/tfsec/internal/app/tfsec/adapter/github/secrets"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 )
 
 func Adapt(modules []block.Module) github.GitHub {
 	return github.GitHub{
-		Repositories: repositories.Adapt(modules),
+		Repositories:       repositories.Adapt(modules),
+		EnvironmentSecrets: secrets.Adapt(modules),
 	}
 }
