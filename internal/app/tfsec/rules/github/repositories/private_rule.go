cbf58c805950da5301753583f09e92106c34f2cf
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-04 12:41:33 +0100

    Github repositories - convert to use defsec (#1210)
    Co-authored-by: Liam Galvin <liam.galvin@aquasec.com>

--- internal/app/tfsec/rules/github/repositories/private_rule.go
+++ internal/app/tfsec/rules/github/repositories/private_rule.go
@@ -1,9 +1,7 @@
 package repositories
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/github/repositories"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -43,33 +41,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"github_repository"},
 		Base:           repositories.CheckPrivate,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-
-			privateAttribute := resourceBlock.GetAttribute("private")
-			visibilityAttribute := resourceBlock.GetAttribute("visibility")
-			if visibilityAttribute.IsNil() && privateAttribute.IsNil() {
-				results.Add("Resource is missing both of `private` or `visibility` attributes - one of these is required to make repository private", resourceBlock)
-				return
-			}
-
-			// this should be evaluated first as visibility overrides private
-			if visibilityAttribute.IsNotNil() {
-				if visibilityAttribute.Equals("public") {
-					results.Add("Resource has visibility set to public - visibility should be set to `private` or `internal` to make repository private", visibilityAttribute)
-				}
-				// stop here as visibility parameter trumps the private one
-				// see https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository
-				return
-			}
-
-			// this should be evaluated first as visibility overrides private
-			if privateAttribute.IsNotNil() {
-				if privateAttribute.IsFalse() {
-					results.Add("Resource has private set to false - it should be set to `true` to make repository private", privateAttribute)
-				}
-			}
-
-			return results
-		},
 	})
 }
