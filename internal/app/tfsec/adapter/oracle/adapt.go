1b1d974f754c145e2ade1ea32b9eaa1480db90d8
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-06 00:58:34 +0100

    Oracle compute - adapt to use defsec (#1231)
    * Oracle compute - adapt to use defsec
    
    * update defsec dependency
    
    Co-authored-by: Liam Galvin <liam.galvin@aquasec.com>

--- internal/app/tfsec/adapter/oracle/adapt.go
+++ internal/app/tfsec/adapter/oracle/adapt.go
@@ -7,6 +7,23 @@ import (
 
 func Adapt(modules []block.Module) oracle.Oracle {
 	return oracle.Oracle{
-		Compute: oracle.Compute{},
+		Compute: adaptCompute(modules),
 	}
+}
+
+func adaptCompute(modules []block.Module) oracle.Compute {
+	var compute oracle.Compute
+
+	for _, module := range modules {
+		for _, resource := range module.GetResourcesByType("opc_compute_ip_address_reservation") {
+
+			addressPoolAttr := resource.GetAttribute("ip_address_pool")
+			addressPoolVal := addressPoolAttr.AsStringValueOrDefault("", resource)
+
+			compute.AddressReservations = append(compute.AddressReservations, oracle.AddressReservation{
+				Pool: addressPoolVal,
+			})
+		}
+	}
+	return compute
 }
