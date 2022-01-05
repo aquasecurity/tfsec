d9e1bc5b3ba76102f17d995a9ec15064acf13d3e
Mads Jensen <mje@inducks.org>
2022-01-05 12:33:47 +0100

    Add test in tfsec

--- internal/app/tfsec/rules/azure/database/retention_period_set_rule_test.go
+++ internal/app/tfsec/rules/azure/database/retention_period_set_rule_test.go
@@ -29,6 +29,19 @@ func Test_AZUDatabaseAuditingRetention90Days(t *testing.T) {
 			mustIncludeResultCode: expectedCode,
 		},
 		{
+			name: "check passes if retention period is unlimited (0)",
+			source: `
+ resource "azurerm_mssql_database_extended_auditing_policy" "unlimited_retention" {
+   database_id                             = azurerm_mssql_database.example.id
+   storage_endpoint                        = azurerm_storage_account.example.primary_blob_endpoint
+   storage_account_access_key              = azurerm_storage_account.example.primary_access_key
+   storage_account_access_key_is_secondary = false
+   retention_in_days                       = 0
+ }
+ `,
+			mustExcludeResultCode: expectedCode,
+		},
+		{
 			name: "check fails if extended_auditing_policy has retention less than 90 days",
 			source: `
  resource "azurerm_sql_server" "good_example" {
