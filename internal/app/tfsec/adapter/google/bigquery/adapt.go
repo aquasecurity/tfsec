99c6f32a5029dd2697d2257c4850d74c67c011c9
vanesa <vsejdiu4@gmail.com>
2022-01-06 23:34:11 +0100

    Google big query - adapt to use defsec

--- internal/app/tfsec/adapter/google/bigquery/adapt.go
+++ internal/app/tfsec/adapter/google/bigquery/adapt.go
@@ -6,5 +6,48 @@ import (
 )
 
 func Adapt(modules []block.Module) bigquery.BigQuery {
-	return bigquery.BigQuery{}
+	return bigquery.BigQuery{
+		Datasets: adaptDatasets(modules),
+	}
+}
+
+func adaptDatasets(modules []block.Module) []bigquery.Dataset {
+	var datasets []bigquery.Dataset
+	for _, module := range modules {
+		for _, resource := range module.GetResourcesByType("google_bigquery_dataset") {
+			datasets = append(datasets, adaptDataset(resource))
+		}
+	}
+	return datasets
+}
+
+func adaptDataset(resource block.Block) bigquery.Dataset {
+	IDAttr := resource.GetAttribute("dataset_id")
+	IDVal := IDAttr.AsStringValueOrDefault("", resource)
+
+	var accessGrants []bigquery.AccessGrant
+
+	accessBlocks := resource.GetBlocks("access")
+	for _, accessBlock := range accessBlocks {
+		roleAttr := accessBlock.GetAttribute("role")
+		roleVal := roleAttr.AsStringValueOrDefault("", accessBlock)
+
+		domainAttr := accessBlock.GetAttribute("domain")
+		domainVal := domainAttr.AsStringValueOrDefault("", accessBlock)
+
+		specialGrAttr := accessBlock.GetAttribute("special_group")
+		specialGrVal := specialGrAttr.AsStringValueOrDefault("", accessBlock)
+
+		accessGrants = append(accessGrants, bigquery.AccessGrant{
+			Role:         roleVal,
+			Domain:       domainVal,
+			SpecialGroup: specialGrVal,
+		})
+	}
+
+	return bigquery.Dataset{
+		Metadata:     *resource.GetMetadata(),
+		ID:           IDVal,
+		AccessGrants: accessGrants,
+	}
 }
