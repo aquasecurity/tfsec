a79b2367644d439244d5d1eea3e40f1007c9d4e5
Mads Jensen <mje@inducks.org>
2022-01-06 18:04:48 +0100

    Spaces and versioning

--- internal/app/tfsec/adapter/digitalocean/spaces/adapt.go
+++ internal/app/tfsec/adapter/digitalocean/spaces/adapt.go
@@ -7,21 +7,24 @@ import (
 
 func Adapt(modules []block.Module) spaces.Spaces {
 	return spaces.Spaces{
-		Buckets: []spaces.Bucket{},
+		Buckets: adaptBuckets(modules),
 	}
 }
 
-func adaptBuckets(modules block.Modules) []spaces.Bucket {
+func adaptBuckets(modules []block.Module) []spaces.Bucket {
 	var buckets []spaces.Bucket
 	for _, module := range modules {
-		for _, block := range module.GetBlocksByTypeLabel("digitalocean_spaces_bucket") {
+		for _, block := range module.GetResourcesByType("digitalocean_spaces_bucket") {
 			bucket := spaces.Bucket{
 				ACL:          block.GetAttribute("acl").AsStringValueOrDefault("", block),
 				ForceDestroy: block.GetAttribute("force_destroy").AsBoolValueOrDefault(false, block),
 			}
 			versioning := block.GetBlock("versioning")
 			if versioning != nil {
-				bucket.Versioning.Enabled = versioning.GetAttribute("enabled").AsBoolValueOrDefault(false, versioning)
+				v := spaces.Versioning{
+					Enabled: versioning.GetAttribute("enabled").AsBoolValueOrDefault(false, versioning),
+				}
+				bucket.Versioning = v
 			}
 			buckets = append(buckets, bucket)
 		}
