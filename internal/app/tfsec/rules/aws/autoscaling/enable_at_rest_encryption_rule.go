276e8862f7a0fc0fdd9b3b5ba1548b40b5df0106
vanesa <vsejdiu4@gmail.com>
2022-01-08 23:08:28 +0100

    AWS Autoscaling - convert to use defsec

--- internal/app/tfsec/rules/aws/autoscaling/enable_at_rest_encryption_rule.go
+++ internal/app/tfsec/rules/aws/autoscaling/enable_at_rest_encryption_rule.go
@@ -1,9 +1,7 @@
 package autoscaling
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/autoscaling"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -31,39 +29,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"aws_launch_configuration"},
 		Base:           autoscaling.CheckEnableAtRestEncryption,
-		CheckTerraform: func(resourceBlock block.Block, module block.Module) (results rules.Results) {
-
-			var encryptionByDefault bool
-			for _, defaultEncryptionBlock := range module.GetResourcesByType("aws_ebs_encryption_by_default") {
-				enabledAttr := defaultEncryptionBlock.GetAttribute("enabled")
-				if enabledAttr.IsTrue() {
-					encryptionByDefault = true
-				}
-			}
-
-			rootDeviceBlock := resourceBlock.GetBlock("root_block_device")
-			if rootDeviceBlock.IsNil() && !encryptionByDefault {
-				results.Add("Resource uses an unencrypted root EBS block device. Consider adding 'root_block_device{ encrypted = true }'", resourceBlock)
-			} else if rootDeviceBlock.IsNotNil() {
-				results = append(results, checkDeviceEncryption(rootDeviceBlock, encryptionByDefault, resourceBlock)...)
-			}
-
-			ebsDeviceBlocks := resourceBlock.GetBlocks("ebs_block_device")
-			for _, ebsDeviceBlock := range ebsDeviceBlocks {
-				results = append(results, checkDeviceEncryption(ebsDeviceBlock, encryptionByDefault, resourceBlock)...)
-			}
-
-			return results
-		},
 	})
-}
-
-func checkDeviceEncryption(deviceBlock block.Block, encryptionByDefault bool, resourceBlock block.Block) (results rules.Results) {
-	encryptedAttr := deviceBlock.GetAttribute("encrypted")
-	if encryptedAttr.IsNil() && !encryptionByDefault {
-		results.Add("Resource uses an unencrypted EBS block device. Consider adding 'encrypted = true'", deviceBlock)
-	} else if encryptedAttr.IsFalse() {
-		results.Add("Resource uses an unencrypted root EBS block device.", encryptedAttr)
-	}
-	return results
 }
--- internal/app/tfsec/rules/aws/autoscaling/no_sensitive_info_rule.go
+++ internal/app/tfsec/rules/aws/autoscaling/no_sensitive_info_rule.go
@@ -1,15 +1,9 @@
 package autoscaling
 
 import (
-	"encoding/base64"
-
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/autoscaling"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/debug"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
-	"github.com/owenrumney/squealer/pkg/squealer"
 )
 
 func init() {
@@ -59,38 +53,5 @@ func init() {
 			"aws_launch_configuration",
 		},
 		Base: autoscaling.CheckNoSensitiveInfo,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-
-			if resourceBlock.MissingChild("user_data") && resourceBlock.MissingChild("user_data_base64") {
-				return
-			}
-
-			customDataAttr := resourceBlock.GetAttribute("user_data")
-			customDataBase64Attr := resourceBlock.GetAttribute("user_data_base64")
-
-			if customDataAttr.IsNotNil() {
-				for _, str := range customDataAttr.ValueAsStrings() {
-					if checkStringForSensitive(str) {
-						results.Add("Resource has user_data with sensitive data.", customDataAttr)
-					}
-				}
-			}
-
-			if customDataBase64Attr.IsNotNil() && customDataBase64Attr.IsString() {
-				encoded, err := base64.StdEncoding.DecodeString(customDataBase64Attr.Value().AsString())
-				if err != nil {
-					debug.Log("could not decode the base64 string in the terraform, trying with the string verbatim")
-					encoded = []byte(customDataAttr.Value().AsString())
-				}
-				if checkStringForSensitive(string(encoded)) {
-					results.Add("Resource has user_data_base64 with sensitive data.", customDataBase64Attr)
-				}
-			}
-			return results
-		},
 	})
-}
-
-func checkStringForSensitive(stringToCheck string) bool {
-	return squealer.NewStringScanner().Scan(stringToCheck).TransgressionFound
 }
