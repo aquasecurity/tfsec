707359b41e1a9da054b7a3d7a36e1bb138292612
vanesa <vsejdiu4@gmail.com>
2022-01-09 02:48:04 +0100

    AWS Kinesis - adapt to use defsec

--- internal/app/tfsec/adapter/aws/kinesis/adapt.go
+++ internal/app/tfsec/adapter/aws/kinesis/adapt.go
@@ -6,5 +6,33 @@ import (
 )
 
 func Adapt(modules []block.Module) kinesis.Kinesis {
-	return kinesis.Kinesis{}
+	return kinesis.Kinesis{
+		Streams: adaptStreams(modules),
+	}
+}
+
+func adaptStreams(modules []block.Module) []kinesis.Stream {
+	var streams []kinesis.Stream
+	for _, module := range modules {
+		for _, resource := range module.GetResourcesByType("aws_kinesis_stream") {
+			streams = append(streams, adaptStream(resource))
+		}
+	}
+	return streams
+}
+
+func adaptStream(resource block.Block) kinesis.Stream {
+	encryptionTypeAttr := resource.GetAttribute("encryption_type")
+	encryptionTypeVal := encryptionTypeAttr.AsStringValueOrDefault("NONE", resource)
+
+	KMSKeyIDAttr := resource.GetAttribute("kms_key_id")
+	KMSKeyIDVal := KMSKeyIDAttr.AsStringValueOrDefault("", resource)
+
+	return kinesis.Stream{
+		Metadata: *resource.GetMetadata(),
+		Encryption: kinesis.Encryption{
+			Type:     encryptionTypeVal,
+			KMSKeyID: KMSKeyIDVal,
+		},
+	}
 }
--- internal/app/tfsec/rules/aws/kinesis/enable_in_transit_encryption_rule.go
+++ internal/app/tfsec/rules/aws/kinesis/enable_in_transit_encryption_rule.go
@@ -1,15 +1,10 @@
 package kinesis
 
 import (
-	"strings"
-
 	"github.com/aquasecurity/defsec/rules/aws/kinesis"
 
-	"github.com/aquasecurity/defsec/rules"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
-	"github.com/zclconf/go-cty/cty"
 )
 
 func init() {
@@ -32,21 +27,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"aws_kinesis_stream"},
 		Base:           kinesis.CheckEnableInTransitEncryption,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-			encryptionTypeAttr := resourceBlock.GetAttribute("encryption_type")
-			if encryptionTypeAttr.IsNil() {
-				results.Add("Resource defines an unencrypted Kinesis Stream.", resourceBlock)
-			} else if encryptionTypeAttr.Type() == cty.String && strings.ToUpper(encryptionTypeAttr.Value().AsString()) != "KMS" {
-				results.Add("Resource defines an unencrypted Kinesis Stream.", encryptionTypeAttr)
-			} else {
-				keyIDAttr := resourceBlock.GetAttribute("kms_key_id")
-				if keyIDAttr.IsNil() {
-					results.Add("Resource defines a Kinesis Stream encrypted with the default Kinesis key.", resourceBlock)
-				} else if keyIDAttr.IsEmpty() || keyIDAttr.Equals("alias/aws/kinesis") {
-					results.Add("Resource defines a Kinesis Stream encrypted with the default Kinesis key.", keyIDAttr)
-				}
-			}
-			return results
-		},
 	})
 }
