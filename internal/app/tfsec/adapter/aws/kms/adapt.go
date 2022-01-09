a473915496ec537ebfe7aead5220c7dbd7456c26
vanesa <vsejdiu4@gmail.com>
2022-01-09 03:39:48 +0100

    AWS KMS - adapt to use defsec

--- internal/app/tfsec/adapter/aws/kms/adapt.go
+++ internal/app/tfsec/adapter/aws/kms/adapt.go
@@ -6,5 +6,31 @@ import (
 )
 
 func Adapt(modules []block.Module) kms.KMS {
-	return kms.KMS{}
+	return kms.KMS{
+		Keys: adaptKeys(modules),
+	}
+}
+
+func adaptKeys(modules []block.Module) []kms.Key {
+	var keys []kms.Key
+	for _, module := range modules {
+		for _, resource := range module.GetResourcesByType("aws_kms_key") {
+			keys = append(keys, adaptKey(resource))
+		}
+	}
+	return keys
+}
+
+func adaptKey(resource block.Block) kms.Key {
+	usageAttr := resource.GetAttribute("key_usage")
+	usageVal := usageAttr.AsStringValueOrDefault("ENCRYPT_DECRYPT", resource)
+
+	enableKeyRotationAttr := resource.GetAttribute("enable_key_rotation")
+	enableKeyRotationVal := enableKeyRotationAttr.AsBoolValueOrDefault(false, resource)
+
+	return kms.Key{
+		Metadata:        *resource.GetMetadata(),
+		Usage:           usageVal,
+		RotationEnabled: enableKeyRotationVal,
+	}
 }
