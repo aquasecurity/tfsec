7f5a7cf9ded42d638c8f53aefdc10e8f231b3d26
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-11 12:40:05 +0100

    AWS Config - adapt to use defsec (#1274)
    Co-authored-by: Owen Rumney <owen.rumney@aquasec.com>

--- internal/app/tfsec/adapter/aws/config/adapt.go
+++ internal/app/tfsec/adapter/aws/config/adapt.go
@@ -2,9 +2,32 @@ package config
 
 import (
 	"github.com/aquasecurity/defsec/provider/aws/config"
+	"github.com/aquasecurity/defsec/types"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 )
 
 func Adapt(modules []block.Module) config.Config {
-	return config.Config{}
+	return config.Config{
+		ConfigurationAggregrator: adaptConfigurationAggregrator(modules),
+	}
+}
+
+func adaptConfigurationAggregrator(modules []block.Module) config.ConfigurationAggregrator {
+	var configurationAggregrator config.ConfigurationAggregrator
+
+	for _, module := range modules {
+		for _, resource := range module.GetResourcesByType("aws_config_configuration_aggregator") {
+			configurationAggregrator.IsDefined = true
+
+			aggregationBlock := resource.GetFirstMatchingBlock("account_aggregation_source", "organization_aggregation_source")
+			if aggregationBlock.IsNil() {
+				configurationAggregrator.SourceAllRegions = types.Bool(false, *resource.GetMetadata())
+			} else {
+				allRegionsAttr := aggregationBlock.GetAttribute("all_regions")
+				allRegionsVal := allRegionsAttr.AsBoolValueOrDefault(false, aggregationBlock)
+				configurationAggregrator.SourceAllRegions = allRegionsVal
+			}
+		}
+	}
+	return configurationAggregrator
 }
