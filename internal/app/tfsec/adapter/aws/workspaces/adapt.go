32be8df85a81992598b4b9576ae2aa3d03828762
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-11 16:08:18 +0100

    AWS Workspaces - adapt to use defsec (#1309)

--- internal/app/tfsec/adapter/aws/workspaces/adapt.go
+++ internal/app/tfsec/adapter/aws/workspaces/adapt.go
@@ -6,5 +6,39 @@ import (
 )
 
 func Adapt(modules []block.Module) workspaces.WorkSpaces {
-	return workspaces.WorkSpaces{}
+	return workspaces.WorkSpaces{
+		WorkSpaces: adaptWorkspaces(modules),
+	}
+}
+
+func adaptWorkspaces(modules []block.Module) []workspaces.WorkSpace {
+	var workspaces []workspaces.WorkSpace
+	for _, module := range modules {
+		for _, resource := range module.GetResourcesByType("aws_workspaces_workspace") {
+			workspaces = append(workspaces, adaptWorkspace(resource))
+		}
+	}
+	return workspaces
+}
+
+func adaptWorkspace(resource block.Block) workspaces.WorkSpace {
+	rootVolumeEncryptAttr := resource.GetAttribute("root_volume_encryption_enabled")
+	rootVolumeEncryptVal := rootVolumeEncryptAttr.AsBoolValueOrDefault(false, resource)
+
+	userVolumeEncryptAttr := resource.GetAttribute("user_volume_encryption_enabled")
+	userVolumeEncryptVal := userVolumeEncryptAttr.AsBoolValueOrDefault(false, resource)
+
+	return workspaces.WorkSpace{
+		Metadata: *resource.GetMetadata(),
+		RootVolume: workspaces.Volume{
+			Encryption: workspaces.Encryption{
+				Enabled: rootVolumeEncryptVal,
+			},
+		},
+		UserVolume: workspaces.Volume{
+			Encryption: workspaces.Encryption{
+				Enabled: userVolumeEncryptVal,
+			},
+		},
+	}
 }
