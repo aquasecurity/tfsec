bf4d6ca6734a092a9a895276ae8a52f20322b08b
vanesa <vsejdiu4@gmail.com>
2022-01-11 16:00:15 +0100

    AWS Workspaces - adapt to use defsec

--- internal/app/tfsec/rules/aws/workspaces/enable_disk_encryption_rule.go
+++ internal/app/tfsec/rules/aws/workspaces/enable_disk_encryption_rule.go
@@ -1,9 +1,7 @@
 package workspaces
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/workspaces"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -49,28 +47,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"aws_workspaces_workspace"},
 		Base:           workspaces.CheckEnableDiskEncryption,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-
-			if resourceBlock.MissingChild("root_volume_encryption_enabled") {
-				results.Add("Resource should have root volume encryption enabled", resourceBlock)
-			} else {
-				rootVolEncAttr := resourceBlock.GetAttribute("root_volume_encryption_enabled")
-				if rootVolEncAttr.IsNotNil() && rootVolEncAttr.IsFalse() {
-					results.Add("Resource has the root volume encryption set to false", rootVolEncAttr)
-				}
-			}
-
-			if resourceBlock.MissingChild("user_volume_encryption_enabled") {
-				results.Add("Resource should have user volume encryption enabled", resourceBlock)
-				return
-			}
-
-			userVolEncAttr := resourceBlock.GetAttribute("user_volume_encryption_enabled")
-			if userVolEncAttr.IsNotNil() && userVolEncAttr.IsFalse() {
-				results.Add("Resource has the user volume encryption set to false", userVolEncAttr)
-			}
-
-			return results
-		},
 	})
 }
