cf8106759e51cc5838b08832bc9caf8eb966438e
vanesa <vsejdiu4@gmail.com>
2022-01-12 22:54:16 +0100

    AWS DocDB - adapt to use defsec

--- internal/app/tfsec/rules/aws/documentdb/enable_storage_encryption_rule.go
+++ internal/app/tfsec/rules/aws/documentdb/enable_storage_encryption_rule.go
@@ -1,9 +1,7 @@
 package documentdb
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/documentdb"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -44,13 +42,5 @@ func init() {
 			"aws_docdb_cluster",
 		},
 		Base: documentdb.CheckEnableStorageEncryption,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-			if storageEncryptedAttr := resourceBlock.GetAttribute("storage_encrypted"); storageEncryptedAttr.IsNil() { // alert on use of default value
-				results.Add("Resource uses default value for storage_encrypted", resourceBlock)
-			} else if storageEncryptedAttr.IsFalse() {
-				results.Add("Resource does not have storage_encrypted set to true", storageEncryptedAttr)
-			}
-			return results
-		},
 	})
 }
