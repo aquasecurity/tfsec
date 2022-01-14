62878d007369f41c6bbed50a4ee8d9e6491d3c7f
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-14 11:18:10 +0100

    AWS DocDB - adapt to use defsec (#1325)

--- internal/app/tfsec/rules/aws/documentdb/enable_log_export_rule.go
+++ internal/app/tfsec/rules/aws/documentdb/enable_log_export_rule.go
@@ -1,9 +1,7 @@
 package documentdb
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/documentdb"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -44,13 +42,5 @@ func init() {
 			"aws_docdb_cluster",
 		},
 		Base: documentdb.CheckEnableLogExport,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-			if enabledCloudwatchLogsExportsAttr := resourceBlock.GetAttribute("enabled_cloudwatch_logs_exports"); enabledCloudwatchLogsExportsAttr.IsNil() { // alert on use of default value
-				results.Add("Resource uses default value for enabled_cloudwatch_logs_exports", resourceBlock)
-			} else if enabledCloudwatchLogsExportsAttr.IsNotAny("audit", "profiler") {
-				results.Add("Resource does not have enabled_cloudwatch_logs_exports set to one of [audit profiler]", enabledCloudwatchLogsExportsAttr)
-			}
-			return results
-		},
 	})
 }
