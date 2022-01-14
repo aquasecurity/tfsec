2bff6dc58794e15937b3dbb2a157db95ea11310b
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-14 10:04:52 +0100

    AWS SQS adapt to use defsec (#1320)

--- internal/app/tfsec/rules/aws/sqs/no_wildcards_in_policy_documents_rule.go
+++ internal/app/tfsec/rules/aws/sqs/no_wildcards_in_policy_documents_rule.go
@@ -1,13 +1,8 @@
 package sqs
 
 import (
-	"encoding/json"
-	"strings"
-
 	"github.com/aquasecurity/defsec/rules/aws/sqs"
 
-	"github.com/aquasecurity/defsec/rules"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -55,30 +50,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"aws_sqs_queue_policy"},
 		Base:           sqs.CheckNoWildcardsInPolicyDocuments,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-
-			if resourceBlock.MissingChild("policy") || !resourceBlock.GetAttribute("policy").IsString() {
-				return
-			}
-
-			policyAttr := resourceBlock.GetAttribute("policy")
-			rawJSON := []byte(policyAttr.Value().AsString())
-			var policy struct {
-				Statement []struct {
-					Effect string `json:"Effect"`
-					Action string `json:"Action"`
-				} `json:"Statement"`
-			}
-
-			if err := json.Unmarshal(rawJSON, &policy); err == nil {
-				for _, statement := range policy.Statement {
-					if strings.ToLower(statement.Effect) == "allow" && (statement.Action == "*" || statement.Action == "sqs:*") {
-						results.Add("SQS policy has a wildcard action specified.", policyAttr)
-					}
-				}
-			}
-
-			return results
-		},
 	})
 }
