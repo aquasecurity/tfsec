8ed376970216d01ef76938260d15480d903f3487
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-14 11:17:25 +0100

    AWS KMS - adapt to use defsec (#1324)

--- internal/app/tfsec/rules/aws/kms/auto_rotate_keys_rule.go
+++ internal/app/tfsec/rules/aws/kms/auto_rotate_keys_rule.go
@@ -1,12 +1,9 @@
 package kms
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/kms"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
-	"github.com/zclconf/go-cty/cty"
 )
 
 func init() {
@@ -28,24 +25,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"aws_kms_key"},
 		Base:           kms.CheckAutoRotateKeys,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-			keyUsageAttr := resourceBlock.GetAttribute("key_usage")
-
-			if keyUsageAttr.IsNotNil() && keyUsageAttr.Equals("SIGN_VERIFY") {
-				return
-			}
-
-			keyRotationAttr := resourceBlock.GetAttribute("enable_key_rotation")
-			if keyRotationAttr.IsNil() {
-				results.Add("Resource does not have KMS Key auto-rotation enabled.", resourceBlock)
-				return
-			}
-
-			if keyRotationAttr.Type() == cty.Bool && keyRotationAttr.Value().False() {
-				results.Add("Resource does not have KMS Key auto-rotation enabled.", keyRotationAttr)
-			}
-
-			return results
-		},
 	})
 }
