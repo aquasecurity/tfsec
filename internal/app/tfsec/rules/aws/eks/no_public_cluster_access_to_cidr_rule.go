ddb166dcb42b347f00b7df6341fc68d6d112afaa
vanesasejdiu <42342975+vanesasejdiu@users.noreply.github.com>
2022-01-14 16:33:12 +0100

    AWS EKS adapt to use defsec (#1331)
    * AWS EKS adapt to use defsec
    
    * Pull latest defsec version
    
    Co-authored-by: Liam Galvin <liam.galvin@aquasec.com>

--- internal/app/tfsec/rules/aws/eks/no_public_cluster_access_to_cidr_rule.go
+++ internal/app/tfsec/rules/aws/eks/no_public_cluster_access_to_cidr_rule.go
@@ -1,10 +1,7 @@
 package eks
 
 import (
-	"github.com/aquasecurity/defsec/rules"
 	"github.com/aquasecurity/defsec/rules/aws/eks"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/block"
-	"github.com/aquasecurity/tfsec/internal/app/tfsec/cidr"
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/scanner"
 	"github.com/aquasecurity/tfsec/pkg/rule"
 )
@@ -41,26 +38,5 @@ func init() {
 		RequiredTypes:  []string{"resource"},
 		RequiredLabels: []string{"aws_eks_cluster"},
 		Base:           eks.CheckNoPublicClusterAccessToCidr,
-		CheckTerraform: func(resourceBlock block.Block, _ block.Module) (results rules.Results) {
-
-			if resourceBlock.MissingChild("vpc_config") {
-				return
-			}
-			vpcConfig := resourceBlock.GetBlock("vpc_config")
-
-			publicAccessEnabledAttr := vpcConfig.GetAttribute("endpoint_public_access")
-			if publicAccessEnabledAttr.IsNotNil() && publicAccessEnabledAttr.IsFalse() {
-				return
-			}
-
-			publicAccessCidrsAttr := vpcConfig.GetAttribute("public_access_cidrs")
-			if publicAccessCidrsAttr.IsNil() {
-				results.Add("Resource uses the default public access cidr of 0.0.0.0/0", vpcConfig)
-			} else if cidr.IsAttributeOpen(publicAccessCidrsAttr) {
-				results.Add("Resource has public access cidr explicitly set to wide open", publicAccessCidrsAttr)
-			}
-
-			return results
-		},
 	})
 }
