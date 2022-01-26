98d79633fb504b7c742f6835df70616594e78fc6
Liam Galvin <liam.galvin@aquasec.com>
2022-01-26 11:53:36 +0000

    tidy up test files

--- internal/pkg/adapter/testutils/loader.go
+++ internal/pkg/adapter/testutils/loader.go
@@ -11,22 +11,21 @@ import (
 )
 
 func CreateModulesFromSource(source string, ext string, t *testing.T) block.Modules {
-	path := CreateTestFile("test"+ext, source)
-	modules, err := parser.New(filepath.Dir(path), parser.OptionStopOnHCLError()).ParseDirectory()
-	if err != nil {
-		t.Fatalf("parse error: %s", err)
-	}
-	return modules
-}
-
-func CreateTestFile(filename, contents string) string {
 	dir, err := ioutil.TempDir(os.TempDir(), "tfsec")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
-	path := filepath.Join(dir, filename)
-	if err := ioutil.WriteFile(path, []byte(contents), 0600); err != nil {
-		panic(err)
+	path := filepath.Join(dir, "test"+ext)
+	if err := ioutil.WriteFile(path, []byte(source), 0600); err != nil {
+		t.Fatal(err)
+
 	}
-	return path
+	defer func() {
+		_ = os.Remove(path)
+	}()
+	modules, err := parser.New(filepath.Dir(path), parser.OptionStopOnHCLError()).ParseDirectory()
+	if err != nil {
+		t.Errorf("parse error: %s", err)
+	}
+	return modules
 }
