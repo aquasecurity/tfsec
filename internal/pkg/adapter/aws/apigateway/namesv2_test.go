c9b0cdc1f4834f17fe3e7dd2d75cd2c7e30b2246
Liam Galvin <liam.galvin@aquasec.com>
2022-01-26 13:33:33 +0000

    add test skeletons

--- internal/pkg/adapter/aws/apigateway/namesv2_test.go
+++ internal/pkg/adapter/aws/apigateway/namesv2_test.go
@@ -15,17 +15,34 @@ func Test_adaptDomainNamesV2(t *testing.T) {
 		expected  []apigateway.DomainName
 	}{
 		{
-			name: "basic",
+			name: "defaults",
+			terraform: `
+resource "aws_apigatewayv2_domain_name" "example" {
+}
+`,
+			expected: []apigateway.DomainName{
+				{
+					Name:           types.String("", types.NewTestMetadata()),
+					Version:        types.Int(2, types.NewTestMetadata()),
+					SecurityPolicy: types.String("TLS_1_0", types.NewTestMetadata()),
+				},
+			},
+		},
+		{
+			name: "fully populated",
 			terraform: `
 resource "aws_apigatewayv2_domain_name" "example" {
                 domain_name = "testing.com"
+                domain_name_configuration {
+                    security_policy = "TLS_1_2"
+                }
 }
 `,
 			expected: []apigateway.DomainName{
 				{
 					Name:           types.String("testing.com", types.NewTestMetadata()),
 					Version:        types.Int(2, types.NewTestMetadata()),
-					SecurityPolicy: types.String("TLS_1_0", types.NewTestMetadata()),
+					SecurityPolicy: types.String("TLS_1_2", types.NewTestMetadata()),
 				},
 			},
 		},
