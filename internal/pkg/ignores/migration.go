1b5a1dac761abc3be2653a2cc283ca302341262e
Liam Galvin <liam.galvin@aquasec.com>
2022-02-02 14:26:17 +0000

    feat: add renamed rules to migration

--- internal/pkg/ignores/migration.go
+++ internal/pkg/ignores/migration.go
@@ -19,9 +19,39 @@ type migrationStatistic struct {
 
 type MigrationStatistics []*migrationStatistic
 
+var renamedMap = map[string]string{
+	"aws-elastic-search-encrypt-replication-group":                    "aws-elasticache-enable-at-rest-encryption",
+	"aws-elastic-service-enable-domain-encryption":                    "aws-elastic-search-enable-domain-encryption",
+	"aws-elbv2-alb-not-public":                                        "aws-elb-alb-not-public",
+	"aws-elbv2-http-not-used":                                         "aws-elb-http-not-used",
+	"aws-rds-backup-retention-specified":                              "aws-rds-specify-backup-retention",
+	"aws-redshift-non-default-vpc-deployment":                         "aws-redshift-use-vpc",
+	"aws-workspace-enable-disk-encryption":                            "aws-workspaces-enable-disk-encryption",
+	"azure-appservice-enable-https-only":                              "azure-appservice-enforce-https",
+	"azure-database-postgres-configuration-log-connection-throttling": "azure-database-postgres-configuration-connection-throttling",
+	"azure-mssql-all-threat-alerts-enabled":                           "azure-database-all-threat-alerts-enabled",
+	"azure-mssql-threat-alert-email-set":                              "azure-database-threat-alert-email-set",
+	"azure-mssql-threat-alert-email-to-owner":                         "azure-database-threat-alert-email-to-owner",
+	"digitalocean-droplet-use-ssh-keys":                               "digitalocean-compute-use-ssh-keys",
+	"digitalocean-loadbalancing-enforce-https":                        "digitalocean-compute-enforce-https",
+	"general-secrets-sensitive-in-attribute":                          "general-secrets-no-plaintext-exposure",
+	"general-secrets-sensitive-in-attribute-value":                    "general-secrets-no-plaintext-exposure",
+	"general-secrets-sensitive-in-local":                              "general-secrets-no-plaintext-exposure",
+	"general-secrets-sensitive-in-variable":                           "general-secrets-no-plaintext-exposure",
+	"google-compute-enable-shielded-vm":                               "google-compute-enable-shielded-vm-im",
+	"google-compute-no-plaintext-disk-keys":                           "google-compute-disk-encryption-no-plaintext-key",
+	"google-compute-no-plaintext-vm-disk-keys":                        "google-compute-disk-encryption-no-plaintext-key",
+	"google-gke-no-legacy-auth":                                       "google-gke-no-legacy-authentication",
+	"google-project-no-default-network":                               "google-iam-no-default-network",
+	"openstack-fw-no-public-access":                                   "openstack-compute-no-public-access",
+}
+
 func RunMigration(dir string) (MigrationStatistics, error) {
 
-	legacyMappings := legacy.IDs
+	legacyMappings := renamedMap
+	for from, to := range legacy.IDs {
+		legacyMappings[from] = to
+	}
 	file, err := os.Stat(dir)
 	if err != nil {
 		return nil, err
@@ -80,7 +110,10 @@ func migrateFile(file string, legacyMapping map[string]string) (MigrationStatist
 
 	for _, match := range matches {
 		legacyCode := match[1]
-		newCode := legacy.IDs[legacyCode]
+		newCode, ok := legacyMapping[legacyCode]
+		if !ok {
+			continue
+		}
 		debug.Log("Found %s, migrating to %s", legacyCode, newCode)
 		contentString = strings.ReplaceAll(contentString, legacyCode, newCode)
 		stats = append(stats, &migrationStatistic{
