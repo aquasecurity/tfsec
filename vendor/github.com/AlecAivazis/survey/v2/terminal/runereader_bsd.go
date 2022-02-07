05d12044f237bb0202ad06d56904de802a3f9d55
gabrielleecredera <92097847+gabrielleecredera@users.noreply.github.com>
2022-02-07 08:33:54 +0000

    feat: add basic `tfsec-checkgen generate` cmd (#1468)
    * feat: add basic `tfsec-checkgen generate` cmd
    
    * chore: add ans to spelling list
    
    Co-authored-by: Owen Rumney <owen@owenrumney.co.uk>

--- vendor/github.com/AlecAivazis/survey/v2/terminal/runereader_bsd.go
+++ vendor/github.com/AlecAivazis/survey/v2/terminal/runereader_bsd.go
@@ -1,13 +1,13 @@
+// copied from: https://github.com/golang/crypto/blob/master/ssh/terminal/util_bsd.go
 // Copyright 2013 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin || dragonfly || freebsd || netbsd || openbsd
 // +build darwin dragonfly freebsd netbsd openbsd
 
-package term
+package terminal
 
-import "golang.org/x/sys/unix"
+import "syscall"
 
-const ioctlReadTermios = unix.TIOCGETA
-const ioctlWriteTermios = unix.TIOCSETA
+const ioctlReadTermios = syscall.TIOCGETA
+const ioctlWriteTermios = syscall.TIOCSETA
