34b5cfebe10a2cb96330f1fb6e4e2fcefb4dc67a
Owen Rumney <owen.rumney@aquasec.com>
2022-02-18 14:05:38 +0000

    chore: remove vendor folder

--- vendor/github.com/aquasecurity/defsec/provider/google/compute/metadata.go
+++
@@ -1,8 +0,0 @@
-package compute
-
-import "github.com/aquasecurity/trivy-config-parsers/types"
-
-type ProjectMetadata struct {
-	types.Metadata
-	EnableOSLogin types.BoolValue
-}
--- vendor/github.com/aquasecurity/defsec/provider/google/compute/network.go
+++
@@ -1,9 +0,0 @@
-package compute
-
-import "github.com/aquasecurity/trivy-config-parsers/types"
-
-type Network struct {
-	types.Metadata
-	Firewall    *Firewall
-	Subnetworks []SubNetwork
-}
