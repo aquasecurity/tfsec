debe844688f77e916389e3926dd54b2961bf238a
Liam Galvin <liam.galvin@aquasec.com>
2022-03-02 11:15:46 +0000

    fix: Fix regression with --exclude-path

--- internal/app/tfsec/cmd/flags.go
+++ internal/app/tfsec/cmd/flags.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
+    "os"
+    "path/filepath"
+    "strings"
 
-	"github.com/aquasecurity/tfsec/pkg/scanner"
+    "github.com/aquasecurity/tfsec/pkg/scanner"
 )
 
 var showVersion = false
@@ -35,78 +35,78 @@ var disableGrouping bool
 var debug bool
 
 func init() {
-	rootCmd.Flags().BoolVar(&singleThreadedMode, "single-thread", singleThreadedMode, "Run checks using a single thread")
-	rootCmd.Flags().BoolVarP(&disableGrouping, "disable-grouping", "G", disableGrouping, "Disable grouping of similar results")
-	rootCmd.Flags().BoolVar(&ignoreHCLErrors, "ignore-hcl-errors", ignoreHCLErrors, "Stop and report an error if an HCL parse error is encountered")
-	rootCmd.Flags().BoolVar(&disableColours, "no-colour", disableColours, "Disable coloured output")
-	rootCmd.Flags().BoolVar(&disableColours, "no-color", disableColours, "Disable colored output (American style!)")
-	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", showVersion, "Show version information and exit")
-	rootCmd.Flags().BoolVar(&runUpdate, "update", runUpdate, "Update to latest version")
-	rootCmd.Flags().BoolVar(&migrateIgnores, "migrate-ignores", migrateIgnores, "Migrate ignore codes to the new ID structure")
-	rootCmd.Flags().StringVarP(&format, "format", "f", format, "Select output format: default, json, csv, checkstyle, junit, sarif. To use multiple formats, separate with a comma and specify a base output filename with --out. A file will be written for each type. The first format will additionally be written stdout.")
-	rootCmd.Flags().StringVarP(&excludedRuleIDs, "exclude", "e", excludedRuleIDs, "Provide comma-separated list of rule IDs to exclude from run.")
-	rootCmd.Flags().StringVar(&filterResults, "filter-results", filterResults, "Filter results to return specific checks only (supports comma-delimited input).")
-	rootCmd.Flags().BoolVarP(&softFail, "soft-fail", "s", softFail, "Runs checks but suppresses error code")
-	rootCmd.Flags().StringSliceVar(&tfvarsPaths, "tfvars-file", tfvarsPaths, "Path to .tfvars file, can be used multiple times and evaluated in order of specification")
-	rootCmd.Flags().StringSliceVar(&excludePaths, "exclude-path", excludePaths, "Folder path to exclude, can be used multiple times and evaluated in order of specification")
-	rootCmd.Flags().StringVarP(&outputFlag, "out", "O", outputFlag, "Set output file. This filename will have a format descriptor appended if multiple formats are specified with --format")
-	rootCmd.Flags().StringVar(&customCheckDir, "custom-check-dir", customCheckDir, "Explicitly the custom checks dir location")
-	rootCmd.Flags().StringVar(&configFile, "config-file", configFile, "Config file to use during run")
-	rootCmd.Flags().BoolVar(&debug, "debug", debug, "Enable debug logging (same as verbose)")
-	rootCmd.Flags().BoolVar(&debug, "verbose", debug, "Enable verbose logging (same as debug)")
-	rootCmd.Flags().BoolVar(&conciseOutput, "concise-output", conciseOutput, "Reduce the amount of output and no statistics")
-	rootCmd.Flags().BoolVar(&excludeDownloaded, "exclude-downloaded-modules", excludeDownloaded, "Remove results for downloaded modules in .terraform folder")
-	rootCmd.Flags().BoolVar(&includePassed, "include-passed", includePassed, "Include passed checks in the result output")
-	rootCmd.Flags().BoolVar(&includeIgnored, "include-ignored", includeIgnored, "Include ignored checks in the result output")
-	rootCmd.Flags().BoolVar(&allDirs, "force-all-dirs", allDirs, "Don't search for tf files, include everything below provided directory.")
-	rootCmd.Flags().BoolVar(&runStatistics, "run-statistics", runStatistics, "View statistics table of current findings.")
-	rootCmd.Flags().BoolVarP(&stopOnCheckError, "allow-checks-to-panic", "p", stopOnCheckError, "Allow panics to propagate up from rule checking")
-	rootCmd.Flags().StringVarP(&workspace, "workspace", "w", workspace, "Specify a workspace for ignore limits")
-	_ = rootCmd.Flags().MarkHidden("allow-checks-to-panic")
+    rootCmd.Flags().BoolVar(&singleThreadedMode, "single-thread", singleThreadedMode, "Run checks using a single thread")
+    rootCmd.Flags().BoolVarP(&disableGrouping, "disable-grouping", "G", disableGrouping, "Disable grouping of similar results")
+    rootCmd.Flags().BoolVar(&ignoreHCLErrors, "ignore-hcl-errors", ignoreHCLErrors, "Stop and report an error if an HCL parse error is encountered")
+    rootCmd.Flags().BoolVar(&disableColours, "no-colour", disableColours, "Disable coloured output")
+    rootCmd.Flags().BoolVar(&disableColours, "no-color", disableColours, "Disable colored output (American style!)")
+    rootCmd.Flags().BoolVarP(&showVersion, "version", "v", showVersion, "Show version information and exit")
+    rootCmd.Flags().BoolVar(&runUpdate, "update", runUpdate, "Update to latest version")
+    rootCmd.Flags().BoolVar(&migrateIgnores, "migrate-ignores", migrateIgnores, "Migrate ignore codes to the new ID structure")
+    rootCmd.Flags().StringVarP(&format, "format", "f", format, "Select output format: default, json, csv, checkstyle, junit, sarif. To use multiple formats, separate with a comma and specify a base output filename with --out. A file will be written for each type. The first format will additionally be written stdout.")
+    rootCmd.Flags().StringVarP(&excludedRuleIDs, "exclude", "e", excludedRuleIDs, "Provide comma-separated list of rule IDs to exclude from run.")
+    rootCmd.Flags().StringVar(&filterResults, "filter-results", filterResults, "Filter results to return specific checks only (supports comma-delimited input).")
+    rootCmd.Flags().BoolVarP(&softFail, "soft-fail", "s", softFail, "Runs checks but suppresses error code")
+    rootCmd.Flags().StringSliceVar(&tfvarsPaths, "tfvars-file", tfvarsPaths, "Path to .tfvars file, can be used multiple times and evaluated in order of specification")
+    rootCmd.Flags().StringSliceVar(&excludePaths, "exclude-path", excludePaths, "Folder path to exclude, can be used multiple times and evaluated in order of specification")
+    rootCmd.Flags().StringVarP(&outputFlag, "out", "O", outputFlag, "Set output file. This filename will have a format descriptor appended if multiple formats are specified with --format")
+    rootCmd.Flags().StringVar(&customCheckDir, "custom-check-dir", customCheckDir, "Explicitly the custom checks dir location")
+    rootCmd.Flags().StringVar(&configFile, "config-file", configFile, "Config file to use during run")
+    rootCmd.Flags().BoolVar(&debug, "debug", debug, "Enable debug logging (same as verbose)")
+    rootCmd.Flags().BoolVar(&debug, "verbose", debug, "Enable verbose logging (same as debug)")
+    rootCmd.Flags().BoolVar(&conciseOutput, "concise-output", conciseOutput, "Reduce the amount of output and no statistics")
+    rootCmd.Flags().BoolVar(&excludeDownloaded, "exclude-downloaded-modules", excludeDownloaded, "Remove results for downloaded modules in .terraform folder")
+    rootCmd.Flags().BoolVar(&includePassed, "include-passed", includePassed, "Include passed checks in the result output")
+    rootCmd.Flags().BoolVar(&includeIgnored, "include-ignored", includeIgnored, "Include ignored checks in the result output")
+    rootCmd.Flags().BoolVar(&allDirs, "force-all-dirs", allDirs, "Don't search for tf files, include everything below provided directory.")
+    rootCmd.Flags().BoolVar(&runStatistics, "run-statistics", runStatistics, "View statistics table of current findings.")
+    rootCmd.Flags().BoolVarP(&stopOnCheckError, "allow-checks-to-panic", "p", stopOnCheckError, "Allow panics to propagate up from rule checking")
+    rootCmd.Flags().StringVarP(&workspace, "workspace", "w", workspace, "Specify a workspace for ignore limits")
+    _ = rootCmd.Flags().MarkHidden("allow-checks-to-panic")
 }
 
 func configureOptions(dir string) []scanner.Option {
-	var options []scanner.Option
-	options = append(options, scanner.OptionWithSingleThread(singleThreadedMode))
-	options = append(options, scanner.OptionStopOnHCLError(!ignoreHCLErrors))
-	options = append(options, scanner.OptionStopOnRuleErrors(stopOnCheckError))
-	options = append(options, scanner.OptionWithTFVarsPaths(tfvarsPaths))
-	options = append(options, scanner.OptionWithExcludePaths(excludePaths))
-	options = append(options, scanner.OptionSkipDownloaded(excludeDownloaded))
-	options = append(options, scanner.OptionIncludePassed(includePassed))
-	options = append(options, scanner.OptionIncludeIgnored(includeIgnored))
-	options = append(options, scanner.OptionScanAllDirectories(allDirs))
-	options = append(options, scanner.OptionWithWorkspaceName(workspace))
+    var options []scanner.Option
+    options = append(options, scanner.OptionWithSingleThread(singleThreadedMode))
+    options = append(options, scanner.OptionStopOnHCLError(!ignoreHCLErrors))
+    options = append(options, scanner.OptionStopOnRuleErrors(stopOnCheckError))
+    options = append(options, scanner.OptionWithTFVarsPaths(tfvarsPaths))
+    options = append(options, scanner.OptionWithExcludePaths(excludePaths))
+    options = append(options, scanner.OptionSkipDownloaded(excludeDownloaded))
+    options = append(options, scanner.OptionIncludePassed(includePassed))
+    options = append(options, scanner.OptionIncludeIgnored(includeIgnored))
+    options = append(options, scanner.OptionScanAllDirectories(allDirs))
+    options = append(options, scanner.OptionWithWorkspaceName(workspace))
 
-	if filterResults != "" {
-		longIDs := strings.Split(filterResults, ",")
-		options = append(options, scanner.OptionWithIncludeOnlyResults(longIDs))
-	}
+    if filterResults != "" {
+        longIDs := strings.Split(filterResults, ",")
+        options = append(options, scanner.OptionWithIncludeOnlyResults(longIDs))
+    }
 
-	if excludedRuleIDs != "" {
-		options = append(options, scanner.OptionExcludeRules(strings.Split(excludedRuleIDs, ",")))
-	}
+    if excludedRuleIDs != "" {
+        options = append(options, scanner.OptionExcludeRules(strings.Split(excludedRuleIDs, ",")))
+    }
 
-	if configFile != "" {
-		options = append(options, scanner.OptionWithConfigFile(configFile))
-	} else {
-		configDir := filepath.Join(dir, ".tfsec")
-		for _, filename := range []string{"config.json", "config.yml", "config.yaml"} {
-			path := filepath.Join(configDir, filename)
-			if _, err := os.Stat(path); err == nil {
-				options = append(options, scanner.OptionWithConfigFile(path))
-				break
-			}
-		}
-	}
-	if customCheckDir != "" {
-		options = append(options, scanner.OptionWithCustomCheckDir(customCheckDir))
-	} else {
-		customDir := filepath.Join(dir, ".tfsec")
-		options = append(options, scanner.OptionWithCustomCheckDir(customDir))
-	}
-	if debug {
-		options = append(options, scanner.OptionWithDebugWriter(os.Stderr))
-	}
-	return options
+    if configFile != "" {
+        options = append(options, scanner.OptionWithConfigFile(configFile))
+    } else {
+        configDir := filepath.Join(dir, ".tfsec")
+        for _, filename := range []string{"config.json", "config.yml", "config.yaml"} {
+            path := filepath.Join(configDir, filename)
+            if _, err := os.Stat(path); err == nil {
+                options = append(options, scanner.OptionWithConfigFile(path))
+                break
+            }
+        }
+    }
+    if customCheckDir != "" {
+        options = append(options, scanner.OptionWithCustomCheckDir(customCheckDir))
+    } else {
+        customDir := filepath.Join(dir, ".tfsec")
+        options = append(options, scanner.OptionWithCustomCheckDir(customDir))
+    }
+    if debug {
+        options = append(options, scanner.OptionWithDebugWriter(os.Stderr))
+    }
+    return options
 }
--- pkg/scanner/options.go
+++ pkg/scanner/options.go
@@ -1,157 +1,160 @@
 package scanner
 
 import (
-	"fmt"
-	"io"
-	"path/filepath"
-	"strings"
+    "fmt"
+    "github.com/aquasecurity/defsec/rules"
+    "io"
+    "path/filepath"
+    "strings"
 
-	"github.com/aquasecurity/defsec/rules"
-
-	"github.com/aquasecurity/defsec/parsers/terraform/parser"
-	"github.com/aquasecurity/tfsec/internal/pkg/executor"
+    "github.com/aquasecurity/defsec/parsers/terraform/parser"
+    "github.com/aquasecurity/tfsec/internal/pkg/executor"
 )
 
 type Option func(s *Scanner)
 
 func OptionWithConfigFile(path string) Option {
-	return func(s *Scanner) {
-		s.configFile = path
-	}
+    return func(s *Scanner) {
+        s.configFile = path
+    }
 }
 
 func OptionWithCustomCheckDir(dir string) Option {
-	return func(s *Scanner) {
-		s.customCheckDir = dir
-	}
+    return func(s *Scanner) {
+        s.customCheckDir = dir
+    }
 }
 
 func OptionWithDebugWriter(w io.Writer) Option {
-	return func(s *Scanner) {
-		s.debugWriter = w
-		s.executorOpt = append(s.executorOpt, executor.OptionWithDebugWriter(w))
-		s.parserOpt = append(s.parserOpt, parser.OptionWithDebugWriter(w))
-	}
+    return func(s *Scanner) {
+        s.debugWriter = w
+        s.executorOpt = append(s.executorOpt, executor.OptionWithDebugWriter(w))
+        s.parserOpt = append(s.parserOpt, parser.OptionWithDebugWriter(w))
+    }
 }
 
 func OptionIncludePassed(include bool) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionIncludePassed(include))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionIncludePassed(include))
+    }
 }
 
 func OptionIncludeIgnored(include bool) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionIncludeIgnored(include))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionIncludeIgnored(include))
+    }
 }
 
 func OptionExcludeRules(ruleIDs []string) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionExcludeRules(ruleIDs))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionExcludeRules(ruleIDs))
+    }
 }
 
 func OptionIncludeRules(ruleIDs []string) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionIncludeRules(ruleIDs))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionIncludeRules(ruleIDs))
+    }
 }
 
 func OptionStopOnRuleErrors(stop bool) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionStopOnErrors(stop))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionStopOnErrors(stop))
+    }
 }
 
 func OptionWithWorkspaceName(name string) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionWithWorkspaceName(name))
-		s.parserOpt = append(s.parserOpt, parser.OptionWithWorkspaceName(name))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionWithWorkspaceName(name))
+        s.parserOpt = append(s.parserOpt, parser.OptionWithWorkspaceName(name))
+    }
 }
 
 func OptionWithSingleThread(single bool) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionWithSingleThread(single))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionWithSingleThread(single))
+    }
 }
 
 func OptionScanAllDirectories(all bool) Option {
-	return func(s *Scanner) {
-		s.forceAllDirs = all
-	}
+    return func(s *Scanner) {
+        s.forceAllDirs = all
+    }
 }
 
 func OptionWithTFVarsPaths(paths []string) Option {
-	return func(s *Scanner) {
-		s.parserOpt = append(s.parserOpt, parser.OptionWithTFVarsPaths(paths))
-	}
+    return func(s *Scanner) {
+        s.parserOpt = append(s.parserOpt, parser.OptionWithTFVarsPaths(paths))
+    }
 }
 
 func OptionStopOnHCLError(stop bool) Option {
-	return func(s *Scanner) {
-		s.parserOpt = append(s.parserOpt, parser.OptionStopOnHCLError(stop))
-	}
+    return func(s *Scanner) {
+        s.parserOpt = append(s.parserOpt, parser.OptionStopOnHCLError(stop))
+    }
 }
 
 func OptionSkipDownloaded(skip bool) Option {
-	return func(s *Scanner) {
-		if !skip {
-			return
-		}
-		s.executorOpt = append(s.executorOpt, executor.OptionWithResultsFilter(func(results rules.Results) rules.Results {
-			var filtered rules.Results
-			for _, result := range results {
-				if result.Range() == nil {
-					continue
-				}
-				search := fmt.Sprintf("%c.terraform%c", filepath.Separator, filepath.Separator)
-				if !strings.Contains(result.Range().GetFilename(), search) {
-					filtered = append(filtered, result)
-				}
-			}
-			return filtered
-		}))
-	}
+    return func(s *Scanner) {
+        if !skip {
+            return
+        }
+        s.executorOpt = append(s.executorOpt, executor.OptionWithResultsFilter(func(results rules.Results) rules.Results {
+            var filtered rules.Results
+            for _, result := range results {
+                if result.Range() == nil {
+                    continue
+                }
+                search := fmt.Sprintf("%c.terraform%c", filepath.Separator, filepath.Separator)
+                if !strings.Contains(result.Range().GetFilename(), search) {
+                    filtered = append(filtered, result)
+                }
+            }
+            return filtered
+        }))
+    }
 }
 
 func OptionWithExcludePaths(paths []string) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionWithResultsFilter(func(results rules.Results) rules.Results {
-			var filtered rules.Results
-			for _, result := range results {
-				if result.Range() == nil {
-					continue
-				}
-				good := true
-				for _, exclude := range paths {
-					if str, err := filepath.Rel(exclude, result.Range().GetFilename()); err == nil && !strings.HasPrefix(str, "..") {
-						good = false
-						break
-					}
-				}
-				if good {
-					filtered = append(filtered, result)
-				}
-			}
-			return filtered
-		}))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionWithResultsFilter(func(results rules.Results) rules.Results {
+            var filtered rules.Results
+            for _, result := range results {
+                if result.Range() == nil {
+                    continue
+                }
+                good := true
+                for _, exclude := range paths {
+                    abs, err := filepath.Abs(exclude)
+                    if err != nil {
+                        continue
+                    }
+                    if str, err := filepath.Rel(abs, result.Range().GetFilename()); err == nil && !strings.HasPrefix(str, "..") {
+                        good = false
+                        break
+                    }
+                }
+                if good {
+                    filtered = append(filtered, result)
+                }
+            }
+            return filtered
+        }))
+    }
 }
 
 func OptionWithIncludeOnlyResults(ids []string) Option {
-	return func(s *Scanner) {
-		s.executorOpt = append(s.executorOpt, executor.OptionWithResultsFilter(func(results rules.Results) rules.Results {
-			var filtered rules.Results
-			for _, result := range results {
-				for _, ruleID := range ids {
-					if result.Rule().LongID() == ruleID {
-						filtered = append(filtered, result)
-					}
-				}
-			}
-			return filtered
-		}))
-	}
+    return func(s *Scanner) {
+        s.executorOpt = append(s.executorOpt, executor.OptionWithResultsFilter(func(results rules.Results) rules.Results {
+            var filtered rules.Results
+            for _, result := range results {
+                for _, ruleID := range ids {
+                    if result.Rule().LongID() == ruleID {
+                        filtered = append(filtered, result)
+                    }
+                }
+            }
+            return filtered
+        }))
+    }
 }
