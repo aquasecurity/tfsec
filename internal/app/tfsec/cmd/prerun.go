b446d29d306ac17ab9ec652fbabaa05e28751a2b
Liam Galvin <liam.galvin@aquasec.com>
2022-05-06 16:51:42 +0100

    Update internal/app/tfsec/cmd/prerun.go
    Co-authored-by: Owen Rumney <owen.rumney@aquasec.com>

--- internal/app/tfsec/cmd/prerun.go
+++ internal/app/tfsec/cmd/prerun.go
@@ -19,7 +19,7 @@ func prerun(cmd *cobra.Command, args []string) error {
 
 
 	// disable colour if running on windows - colour formatting doesn't work
-	if disableColours || (runtime.GOOS == "windows" && term == "") {
+	if disableColours || (runtime.GOOS == "windows" && os.Getenv("TERM") == "") {
 		tml.DisableFormatting()
 	} else {
 		tml.EnableFormatting()
