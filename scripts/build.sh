5a47a083bdb3bb4ebb952f6ed116afdfb7506763
Owen Rumney <owen.rumney@aquasec.com>
2022-08-16 13:43:20 +0100

    fix: Fix build script
    Resolves #1849 - run build script using `make build`
    
    Alternatively, just use `go build `
    
    Signed-off-by: Owen Rumney <owen.rumney@aquasec.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 BINARY=tfsec
 CHECK_GEN_BINARY=tfsec-checkgen
 TAG=${TRAVIS_TAG:-development}
 GO111MODULE=on
 export CGO_ENABLED=0
-export GOFLAGS=-mod=vendor
 args=(-ldflags "-X github.com/aquasecurity/tfsec/version.Version=${TAG} -s -w -extldflags '-fno-PIC -static'")
 
 mkdir -p bin/darwin
 GOOS=darwin GOARCH=amd64 go build -o bin/darwin/${BINARY}-darwin-amd64 "${args[@]}" ./cmd/tfsec/
-GOOS=darwin GOARCH=amd64 go build -o bin/darwin/${CHECK_GEN_BINARY}-darwin-amd64 "${args[@]}" ./cmd/tfsec-checkgen/
+GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/${CHECK_GEN_BINARY}-darwin-amd64 "${args[@]}" ./cmd/tfsec-checkgen/
 mkdir -p bin/linux
 GOOS=linux GOARCH=amd64 go build -o bin/linux/${BINARY}-linux-amd64 "${args[@]}" ./cmd/tfsec/
 GOOS=linux GOARCH=amd64 go build -o bin/linux/${CHECK_GEN_BINARY}-linux-amd64 "${args[@]}" ./cmd/tfsec-checkgen/
