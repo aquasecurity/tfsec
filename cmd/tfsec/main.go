38b34b4a2bcffb0a025568c93fdaa1ef4e8d84fd
Simar <simar@linux.com>
2023-06-13 17:31:49 -0600

    feat: Add a transition message
    Fixes: https://github.com/aquasecurity/tfsec/issues/2066
    
    Signed-off-by: Simar <simar@linux.com>

--- cmd/tfsec/main.go
+++ cmd/tfsec/main.go
@@ -8,7 +8,21 @@ import (
 	"github.com/aquasecurity/tfsec/internal/app/tfsec/cmd"
 )
 
+const transitionMsg = `
+======================================================
+tfsec is joining the Trivy family
+
+tfsec will continue to remain available 
+for the time being, although our engineering 
+attention will be directed at Trivy going forward.
+
+You can read more here: 
+https://github.com/aquasecurity/tfsec/discussions/1994
+======================================================
+`
+
 func main() {
+	fmt.Print(transitionMsg)
 	if err := cmd.Root().Execute(); err != nil {
 		if err.Error() != "" {
 			fmt.Printf("Error: %s\n", err)
